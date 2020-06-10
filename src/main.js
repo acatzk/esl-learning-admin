33989a998334566aa6e961e761f1cbcfa452a94f
acatzk <joshuaimalay@gmail.com>
2020-06-10 15:20:24 +0800

    Import vue-page-transition

--- src/main.js
+++ src/main.js
@@ -2,8 +2,10 @@ import Vue from "vue";
 import App from "@/App.vue";
 import router from "@/router";
 import store from "@/store";
+import VuePageTransition from "vue-page-transition";
 
 Vue.config.productionTip = false;
+Vue.use(VuePageTransition);
 
 new Vue({
   router,
