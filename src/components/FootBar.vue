f86aaba262a645c7cbdc194f54d65316d305af19
acatzk <joshuaimalay@gmail.com>
2020-06-10 10:28:54 +0800

    Added FootBar vue component

---
+++ src/components/FootBar.vue
@@ -0,0 +1,8 @@
+<template>
+    <footer class="footer">
+        <div class="w-100 clearfix">
+            <span class="text-center text-sm-left d-md-inline-block">Copyright © 2020 Vic Solution, Inc. All Rights Reserved.</span>
+            <span class="float-none float-sm-right mt-1 mt-sm-0 text-center">Crafted with <i class="fa fa-heart text-danger"></i> by <a href="http://lavalite.org/" class="text-dark" target="_blank">Lavalite</a></span>
+        </div>
+    </footer>
+</template>
