14af1577cab04f4ed422c1058cbf5966d4692b33
acatzk <joshuaimalay@gmail.com>
2020-06-10 15:01:57 +0800

    Changed footer paragraph

--- src/components/layouts/FootBar.vue
+++ src/components/layouts/FootBar.vue
@@ -1,8 +1,12 @@
 <template>
     <footer class="footer">
         <div class="w-100 clearfix">
-            <span class="text-center text-sm-left d-md-inline-block">Copyright © 2020 Vic Solution, Inc. All Rights Reserved.</span>
-            <span class="float-none float-sm-right mt-1 mt-sm-0 text-center">Crafted with <i class="fa fa-heart text-danger"></i> by <a href="http://lavalite.org/" class="text-dark" target="_blank">Lavalite</a></span>
+            <span class="text-center text-sm-left d-md-inline-block">Copyright © {{ new Date().getFullYear() }} Vic Solution, Inc. All Rights Reserved.</span>
+            <span class="float-none float-sm-right mt-1 mt-sm-0 text-center">Crafted with <i class="fa fa-heart text-danger"></i> by 
+                <a href="https://github.com/acatzk" target="_blank" class="text-dark">acatzk</a> &
+                <a href="https://github.com/veoscript" target="_blank" class="text-dark">veoscript</a>
+            </span>
+
         </div>
     </footer>
 </template>
