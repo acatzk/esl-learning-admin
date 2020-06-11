48ddd11c98496384418642aa239f47e12986be5b
acatzk <joshuaimalay@gmail.com>
2020-06-11 12:22:08 +0800

    Changed and add onAuthStateChanged function

--- src/main.js
+++ src/main.js
@@ -3,12 +3,19 @@ import App from "@/App.vue";
 import router from "@/router";
 import store from "@/store";
 import VuePageTransition from "vue-page-transition";
+import { fb } from '@/firebase.js'
 
 Vue.config.productionTip = false;
 Vue.use(VuePageTransition);
 
-new Vue({
-  router,
-  store,
-  render: h => h(App)
-}).$mount("#app");
+let app = ''
+
+fb.auth().onAuthStateChanged(() => {
+  if (!app) {
+    new Vue({
+      router,
+      store,
+      render: h => h(App)
+    }).$mount("#app");
+  }
+})
