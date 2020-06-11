011ccd8ae1556a8f653290a45ccc8cde65e14870
acatzk <joshuaimalay@gmail.com>
2020-06-11 08:57:15 +0800

    Removed unnecessary items

--- src/App.vue
+++ src/App.vue
@@ -22,7 +22,10 @@
                     </div>
                     <div class="top-menu d-flex align-items-center">
                         <div class="dropdown">
-                            <a class="nav-link dropdown-toggle" href="#" id="notiDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false"><i class="ik ik-bell"></i><span class="badge bg-danger">3</span></a>
+                            <a class="nav-link dropdown-toggle" href="#" id="notiDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                                <i class="ik ik-bell"></i>
+                                <span class="badge bg-danger">3</span>
+                            </a>
                             <div class="dropdown-menu dropdown-menu-right notification-dropdown" aria-labelledby="notiDropdown">
                                 <h4 class="header">Notifications</h4>
                                 <div class="notifications-wrap">
@@ -57,35 +60,24 @@
                                 <div class="footer"><a href="javascript:void(0);">See all activity</a></div>
                             </div>
                         </div>
-                        <button type="button" class="nav-link ml-10 right-sidebar-toggle"><i class="ik ik-message-square"></i><span class="badge bg-success">3</span></button>
-                        <div class="dropdown">
-                            <a class="nav-link dropdown-toggle" href="#" id="menuDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false"><i class="ik ik-plus"></i></a>
-                            <div class="dropdown-menu dropdown-menu-right menu-grid" aria-labelledby="menuDropdown">
-                                <a class="dropdown-item" href="#" data-toggle="tooltip" data-placement="top" title="Dashboard"><i class="ik ik-bar-chart-2"></i></a>
-                                <a class="dropdown-item" href="#" data-toggle="tooltip" data-placement="top" title="Message"><i class="ik ik-mail"></i></a>
-                                <a class="dropdown-item" href="#" data-toggle="tooltip" data-placement="top" title="Accounts"><i class="ik ik-users"></i></a>
-                                <a class="dropdown-item" href="#" data-toggle="tooltip" data-placement="top" title="Sales"><i class="ik ik-shopping-cart"></i></a>
-                                <a class="dropdown-item" href="#" data-toggle="tooltip" data-placement="top" title="Purchase"><i class="ik ik-briefcase"></i></a>
-                                <a class="dropdown-item" href="#" data-toggle="tooltip" data-placement="top" title="Pages"><i class="ik ik-clipboard"></i></a>
-                                <a class="dropdown-item" href="#" data-toggle="tooltip" data-placement="top" title="Chats"><i class="ik ik-message-square"></i></a>
-                                <a class="dropdown-item" href="#" data-toggle="tooltip" data-placement="top" title="Contacts"><i class="ik ik-map-pin"></i></a>
-                                <a class="dropdown-item" href="#" data-toggle="tooltip" data-placement="top" title="Blocks"><i class="ik ik-inbox"></i></a>
-                                <a class="dropdown-item" href="#" data-toggle="tooltip" data-placement="top" title="Events"><i class="ik ik-calendar"></i></a>
-                                <a class="dropdown-item" href="#" data-toggle="tooltip" data-placement="top" title="Notifications"><i class="ik ik-bell"></i></a>
-                                <a class="dropdown-item" href="#" data-toggle="tooltip" data-placement="top" title="More"><i class="ik ik-more-horizontal"></i></a>
-                            </div>
-                        </div>
-                        <button type="button" class="nav-link ml-10" id="apps_modal_btn" data-toggle="modal" data-target="#appsModal"><i class="ik ik-grid"></i></button>
+                        <button type="button" class="nav-link ml-10 right-sidebar-toggle">
+                            <i class="ik ik-message-square"></i>
+                            <span class="badge bg-success">3</span>
+                        </button>
                         <div class="dropdown">
                             <a class="dropdown-toggle" href="#" id="userDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
                               <img class="avatar" src="@/assets/img/user.jpg" alt="">
                             </a>
                             <div class="dropdown-menu dropdown-menu-right" aria-labelledby="userDropdown">
-                                <a class="dropdown-item" href="profile.html"><i class="ik ik-user dropdown-icon"></i> Profile</a>
-                                <a class="dropdown-item" href="#"><i class="ik ik-settings dropdown-icon"></i> Settings</a>
-                                <a class="dropdown-item" href="#"><span class="float-right"><span class="badge badge-primary">6</span></span><i class="ik ik-mail dropdown-icon"></i> Inbox</a>
-                                <a class="dropdown-item" href="#"><i class="ik ik-navigation dropdown-icon"></i> Message</a>
-                                <a class="dropdown-item" href="login.html"><i class="ik ik-power dropdown-icon"></i> Logout</a>
+                                <router-link to="/profile" class="dropdown-item">
+                                    <i class="ik ik-user dropdown-icon"></i> Profile
+                                </router-link>
+                                <router-link to="/settings" class="dropdown-item">
+                                    <i class="ik ik-settings dropdown-icon"></i> Settings
+                                </router-link>
+                                <a class="dropdown-item" href="#">
+                                    <i class="ik ik-power dropdown-icon"></i> Logout
+                                </a>
                             </div>
                         </div>
 
@@ -197,37 +189,37 @@
                         <div class="list-group row">
                             <a href="javascript:void(0)" class="list-group-item" data-chat-user="Gene Newman">
                                 <figure class="user--online">
-                                    <img src="img/users/1.jpg" class="rounded-circle" alt="">
+                                    <img src="/assets/img/users/1.jpg" class="rounded-circle" alt="">
                                 </figure><span><span class="name">Gene Newman</span>  <span class="username">@gene_newman</span> </span>
                             </a>
                             <a href="javascript:void(0)" class="list-group-item" data-chat-user="Billy Black">
                                 <figure class="user--online">
-                                    <img src="img/users/2.jpg" class="rounded-circle" alt="">
+                                    <img src="/assets/img/users/2.jpg" class="rounded-circle" alt="">
                                 </figure><span><span class="name">Billy Black</span>  <span class="username">@billyblack</span> </span>
                             </a>
                             <a href="javascript:void(0)" class="list-group-item" data-chat-user="Herbert Diaz">
                                 <figure class="user--online">
-                                    <img src="img/users/3.jpg" class="rounded-circle" alt="">
+                                    <img src="/assets/img/users/3.jpg" class="rounded-circle" alt="">
                                 </figure><span><span class="name">Herbert Diaz</span>  <span class="username">@herbert</span> </span>
                             </a>
                             <a href="javascript:void(0)" class="list-group-item" data-chat-user="Sylvia Harvey">
                                 <figure class="user--busy">
-                                    <img src="img/users/4.jpg" class="rounded-circle" alt="">
+                                    <img src="/assets/img/users/4.jpg" class="rounded-circle" alt="">
                                 </figure><span><span class="name">Sylvia Harvey</span>  <span class="username">@sylvia</span> </span>
                             </a>
                             <a href="javascript:void(0)" class="list-group-item active" data-chat-user="Marsha Hoffman">
                                 <figure class="user--busy">
-                                    <img src="img/users/5.jpg" class="rounded-circle" alt="">
+                                    <img src="/assets/img/users/5.jpg" class="rounded-circle" alt="">
                                 </figure><span><span class="name">Marsha Hoffman</span>  <span class="username">@m_hoffman</span> </span>
                             </a>
                             <a href="javascript:void(0)" class="list-group-item" data-chat-user="Mason Grant">
                                 <figure class="user--offline">
-                                    <img src="img/users/1.jpg" class="rounded-circle" alt="">
+                                    <img src="/assets/img/users/1.jpg" class="rounded-circle" alt="">
                                 </figure><span><span class="name">Mason Grant</span>  <span class="username">@masongrant</span> </span>
                             </a>
                             <a href="javascript:void(0)" class="list-group-item" data-chat-user="Shelly Sullivan">
                                 <figure class="user--offline">
-                                    <img src="img/users/2.jpg" class="rounded-circle" alt="">
+                                    <img src="/assets/img/users/2.jpg" class="rounded-circle" alt="">
                                 </figure><span><span class="name">Shelly Sullivan</span>  <span class="username">@shelly</span></span>
                             </a>
                         </div>
@@ -323,90 +315,6 @@
           </div>
       </div>
 
-      <div class="modal fade apps-modal" id="appsModal" tabindex="-1" role="dialog" aria-labelledby="appsModalLabel" aria-hidden="true" data-backdrop="false">
-          <button type="button" class="close" data-dismiss="modal" aria-label="Close"><i class="ik ik-x-circle"></i></button>
-          <div class="modal-dialog modal-dialog-centered" role="document">
-              <div class="modal-content">
-                  <div class="quick-search">
-                      <div class="container">
-                          <div class="row">
-                              <div class="col-md-4 ml-auto mr-auto">
-                                  <div class="input-wrap">
-                                      <input type="text" id="quick-search" class="form-control" placeholder="Search..." />
-                                      <i class="ik ik-search"></i>
-                                  </div>
-                              </div>
-                          </div>
-                      </div>
-                  </div>
-                  <div class="modal-body d-flex align-items-center">
-                      <div class="container">
-                          <div class="apps-wrap">
-                              <div class="app-item">
-                                  <a href="#"><i class="ik ik-bar-chart-2"></i><span>Dashboard</span></a>
-                              </div>
-                              <div class="app-item dropdown">
-                                  <a href="#" class="dropdown-toggle" role="button" id="dropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false"><i class="ik ik-command"></i><span>Ui</span></a>
-                                  <div class="dropdown-menu" aria-labelledby="dropdownMenuLink">
-                                      <a class="dropdown-item" href="#">Action</a>
-                                      <a class="dropdown-item" href="#">Another action</a>
-                                      <a class="dropdown-item" href="#">Something else here</a>
-                                  </div>
-                              </div>
-                              <div class="app-item">
-                                  <a href="#"><i class="ik ik-mail"></i><span>Message</span></a>
-                              </div>
-                              <div class="app-item">
-                                  <a href="#"><i class="ik ik-users"></i><span>Accounts</span></a>
-                              </div>
-                              <div class="app-item">
-                                  <a href="#"><i class="ik ik-shopping-cart"></i><span>Sales</span></a>
-                              </div>
-                              <div class="app-item">
-                                  <a href="#"><i class="ik ik-briefcase"></i><span>Purchase</span></a>
-                              </div>
-                              <div class="app-item">
-                                  <a href="#"><i class="ik ik-server"></i><span>Menus</span></a>
-                              </div>
-                              <div class="app-item">
-                                  <a href="#"><i class="ik ik-clipboard"></i><span>Pages</span></a>
-                              </div>
-                              <div class="app-item">
-                                  <a href="#"><i class="ik ik-message-square"></i><span>Chats</span></a>
-                              </div>
-                              <div class="app-item">
-                                  <a href="#"><i class="ik ik-map-pin"></i><span>Contacts</span></a>
-                              </div>
-                              <div class="app-item">
-                                  <a href="#"><i class="ik ik-box"></i><span>Blocks</span></a>
-                              </div>
-                              <div class="app-item">
-                                  <a href="#"><i class="ik ik-calendar"></i><span>Events</span></a>
-                              </div>
-                              <div class="app-item">
-                                  <a href="#"><i class="ik ik-bell"></i><span>Notifications</span></a>
-                              </div>
-                              <div class="app-item">
-                                  <a href="#"><i class="ik ik-pie-chart"></i><span>Reports</span></a>
-                              </div>
-                              <div class="app-item">
-                                  <a href="#"><i class="ik ik-layers"></i><span>Tasks</span></a>
-                              </div>
-                              <div class="app-item">
-                                  <a href="#"><i class="ik ik-edit"></i><span>Blogs</span></a>
-                              </div>
-                              <div class="app-item">
-                                  <a href="#"><i class="ik ik-settings"></i><span>Settings</span></a>
-                              </div>
-                              <div class="app-item">
-                                  <a href="#"><i class="ik ik-more-horizontal"></i><span>More</span></a>
-                              </div>
-                          </div>
-                      </div>
-                  </div>
-              </div>
-          </div>
-      </div>
   </div>
 </template>
 
