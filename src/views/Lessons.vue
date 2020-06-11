489ab0f58f8e5b52498a2b1a2f393fb0674199c7
acatzk <joshuaimalay@gmail.com>
2020-06-11 11:43:33 +0800

    Import PageHeader vue component

--- src/views/Lessons.vue
+++ src/views/Lessons.vue
@@ -1,3 +1,21 @@
 <template>
-    
+    <div class="lessons">
+        <page-header 
+            title="Lessons"
+            description="Teachers Online lessons"
+            pageIcon="ik book ik-book"
+            currentRouteName="Lessons"
+            titleIcon="ik book ik-book bg-blue"
+        />
+    </div>
 </template>
+
+<script>
+export default {
+    name: 'Lessons',
+
+    components: {
+        PageHeader: () => import('@/components/pages/PageHeader')
+    }
+}
+</script>
