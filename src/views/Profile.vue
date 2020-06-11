7073795494c37a0d8bd254985f4620d9355d9f45
acatzk <joshuaimalay@gmail.com>
2020-06-11 09:05:34 +0800

    Added breadcrumb links of profile section

---
+++ src/views/Profile.vue
@@ -0,0 +1,51 @@
+<template>
+    <div class="profile">
+        <div class="page-header">
+            <div class="row align-items-end">
+                <div class="col-lg-8">
+                    <div class="page-header-title">
+                        <i class="ik ik-file-text bg-blue"></i>
+                        <div class="d-inline">
+                            <h5>Profile</h5>
+                            <span>Administrator Profile Information</span>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-4">
+                    <nav class="breadcrumb-container" aria-label="breadcrumb">
+                        <ol class="breadcrumb">
+                            <li class="breadcrumb-item">
+                                <router-link to="/admin/dashboard">
+                                    <i class="ik ik-home"></i>
+                                </router-link>
+                            </li>
+                            <li class="breadcrumb-item active" aria-current="page">
+                                Profile
+                            </li>
+                        </ol>
+                    </nav>
+                </div>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-md-12">
+                <div class="card">
+                    <div class="card-body">
+                        <div class="row align-items-center">
+                            <div class="col-lg-8 col-md-12">
+                                <h3 class="card-title">Profile</h3>
+                                <div id="visitfromworld" style="width:100%; height:350px"></div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+  name: 'Dashboard'
+}
+</script>
