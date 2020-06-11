55d69e475ad861006730d2a5bb396599a0b8bd10
acatzk <joshuaimalay@gmail.com>
2020-06-11 12:18:48 +0800

    Initialized firebaseConfig

---
+++ src/firebase.js
@@ -0,0 +1,14 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyAE00QgUrP0dR0v2nlvlzxuQgkZL3_j-XM",
+    authDomain: "vic-solution-admin.firebaseapp.com",
+    databaseURL: "https://vic-solution-admin.firebaseio.com",
+    projectId: "vic-solution-admin",
+    storageBucket: "vic-solution-admin.appspot.com",
+    messagingSenderId: "565755394111",
+    appId: "1:565755394111:web:47e5aa6cf7d6e3f100608a",
+    measurementId: "G-JVH39C0YPM"
+};
+
+const fb = firebase.initializeApp(firebaseConfig)
+
+export { fb }
