7cef5b99b412694889ffe577405eac57f1ad3c3e
acatzk <joshuaimalay@gmail.com>
2020-06-12 12:16:30 +0800

    Added style foot-bar

--- src/components/layouts/FootBar.vue
+++ src/components/layouts/FootBar.vue
@@ -1,3 +1,35 @@
 <template>
-    
+  <v-footer
+    padless
+  >
+    <v-card
+      class="flex"
+      flat
+    >
+      <v-card-text class="py-1 text-center">
+        <v-btn
+          v-for="icon in icons"
+          :key="icon"
+          class="mx-4"
+          dark
+          icon
+        >
+          <v-icon size="24px" color="grey">{{ icon }}</v-icon>
+        </v-btn>
+      </v-card-text>
+    </v-card>
+  </v-footer>
 </template>
+
+<script>
+  export default {
+    data: () => ({
+      icons: [
+        'mdi-facebook',
+        'mdi-twitter',
+        'mdi-linkedin',
+        'mdi-instagram',
+      ],
+    }),
+  }
+</script>
