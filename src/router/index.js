0894cdc36463802273d2d7a6b768e5ab8c3b1d0f
acatzk <joshuaimalay@gmail.com>
2020-06-12 15:12:57 +0800

    Added nprogress routes

--- src/router/index.js
+++ src/router/index.js
@@ -34,4 +34,19 @@ const router = new VueRouter({
   mode: 'history'
 });
 
+router.beforeResolve((to, from, next) => {
+  // If this isn't an initial page load.
+  NProgress.configure({ easing: 'ease', speed: 2000, showSpinner: false })
+  if (to.name) {
+    NProgress.start()
+  }
+  next()
+})
+
+router.afterEach((to, from) => {
+  // Complete the animation of the route progress bar.
+  NProgress.done()
+  window.scrollTo(0, 0)
+})
+
 export default router;
