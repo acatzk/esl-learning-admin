9a5d58ce0347820a7fa22ca11744fc0b31022299
acatzk <joshuaimalay@gmail.com>
2020-06-12 15:01:26 +0800

    Added google fonts Nunito

--- src/App.vue
+++ src/App.vue
@@ -3,3 +3,13 @@
     <router-view></router-view>
   </v-app>
 </template>
+
+<style lang="scss">
+@import url("https://fonts.googleapis.com/css?family=Nunito:100,300,400,500,700,900");
+
+body, html {
+  font-family: "Nunito";
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+}
+</style>
