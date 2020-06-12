9abe2a3f023d94db3dcd5537daeb847208d0ce4d
acatzk <joshuaimalay@gmail.com>
2020-06-12 10:47:09 +0800

    Chnaged index lines

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,15 +1,25 @@
 <template>
+
   <div class="home">
+    
     <nav-bar /> <!-- ** NAVIGATION BAR COMPONENT ** -->
+
   </div>
+
 </template>
 
 <script>
 
 export default {
+  
   name: "Home",
+
   components: {
+
     NavBar: () => import('@/components/layouts/NavBar')
+
   }
+
 };
+
 </script>
