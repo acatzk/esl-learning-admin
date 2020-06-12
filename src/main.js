4be91acc4272606f1dfdfa558e9ce4794d3b0da2
acatzk <joshuaimalay@gmail.com>
2020-06-12 13:11:57 +0800

    Import vue-page-transition file

--- src/main.js
+++ src/main.js
@@ -3,11 +3,15 @@ import App from "./App.vue";
 import router from "./router";
 import store from "./store";
 import vuetify from "./plugins/vuetify";
+import VuePageTransition from 'vue-page-transition'
 
 Vue.config.productionTip = false;
 
 /**  VUETIFY ICONS **/
 import 'material-design-icons-iconfont/dist/material-design-icons.css'
+
+/** VUE-PAGE-TRANSITION **/
+Vue.use(VuePageTransition)
 
 new Vue({
   router,
