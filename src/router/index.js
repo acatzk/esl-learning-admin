ab3627e13fe1124e7718a6a5dd92d617af6ebbfc
acatzk <joshuaimalay@gmail.com>
2020-06-12 10:45:38 +0800

    Fixed bug for home route and changed about route into login route

--- src/router/index.js
+++ src/router/index.js
@@ -6,13 +6,13 @@ Vue.use(VueRouter);
 const routes = [
   {
     path: "/",
-    name: "Home",
-    component: import('@/views/Home.vue')
+    name: "home",
+    component: () => import('@/views/Home.vue')
   },
   {
-    path: "/about",
-    name: "About",
-    component: () => import("@/views/About.vue")
+    path: "/login",
+    name: "login",
+    component: () => import("@/views/Login.vue")
   }
 ];
 
