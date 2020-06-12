11e19c42740ff138096e0a58f62691f8966b2ec0
acatzk <joshuaimalay@gmail.com>
2020-06-12 08:49:04 +0800

    Removed files commited

--- src/main.js
+++ src/main.js
@@ -1,27 +1,14 @@
-/* eslint-disable */
-import Vue from 'vue';
-import ShardsVue from 'shards-vue';
-
-// Styles
-import 'bootstrap/dist/css/bootstrap.css';
-import '@/scss/shards-dashboards.scss';
-import '@/assets/scss/date-range.scss';
-
-// Core
-import App from './App.vue';
-import router from './router';
-
-// Layouts
-import Default from '@/layouts/Default.vue';
-
-ShardsVue.install(Vue);
-
-Vue.component('default-layout', Default);
+import Vue from "vue";
+import App from "./App.vue";
+import router from "./router";
+import store from "./store";
+import vuetify from "./plugins/vuetify";
 
 Vue.config.productionTip = false;
-Vue.prototype.$eventHub = new Vue();
 
 new Vue({
   router,
-  render: h => h(App),
-}).$mount('#app');
+  store,
+  vuetify,
+  render: h => h(App)
+}).$mount("#app");
