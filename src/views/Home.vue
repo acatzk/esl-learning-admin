a102463349ef44c92c8ad6987512ff21c3ba2745
acatzk <joshuaimalay@gmail.com>
2020-06-12 10:15:05 +0800

    Import NavBar vue component

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,15 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <nav-bar /> <!-- ** NAVIGATION BAR COMPONENT ** -->
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
 
 export default {
   name: "Home",
   components: {
-    HelloWorld
+    NavBar: () => import('@/components/layouts/NavBar')
   }
 };
 </script>
