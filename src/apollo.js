a7b004b9baafd5658e467fab378d9324fd48f82d
acatzk <joshuaimalay@gmail.com>
2020-06-13 16:52:47 +0800

    Setup vue apollo connections

---
+++ src/apollo.js
@@ -0,0 +1,58 @@
+import Vue from 'vue'
+
+import VueApollo from "vue-apollo";
+import { ApolloClient } from "apollo-client";
+import { HttpLink } from "apollo-link-http";
+import { InMemoryCache } from "apollo-cache-inmemory";
+
+
+import { split } from 'apollo-link'
+import { WebSocketLink } from 'apollo-link-ws'
+import { getMainDefinition } from 'apollo-utilities'
+
+
+const httpLink = new HttpLink({
+  uri: "https://vic-solution-inc.herokuapp.com/v1/graphql",
+  headers: {
+    'x-hasura-admin-secret': 'vic-solution-inc'
+  }
+});
+
+const wsLink = new WebSocketLink({
+  uri: "wss://graphql-data.herokuapp.com/v1/graphql",
+  options: {
+    timeout: 60000,
+    reconnect: true,
+    timeout: 30000,
+    connectionParams() {
+      return {
+        headers: {
+          'x-hasura-admin-secret': 'ilusmdm'
+        }
+      }
+    }
+  }
+})
+
+const link = split(({ query }) => {
+  const { kind, operation } = getMainDefinition(query)
+  return kind === 'OperationDefinition' && operation === 'subscription'
+}, wsLink, httpLink)
+
+
+
+const apolloClient = new ApolloClient({
+    link: link,
+    cache: new InMemoryCache()
+  });
+  
+  Vue.use(VueApollo);
+
+  const apolloProvider = new VueApollo({
+    defaultClient: apolloClient,
+    defaultOptions: {
+      $loadingKey: "loading"
+    }
+  });
+
+  export default apolloProvider
