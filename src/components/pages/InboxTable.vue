9009bf083218fbda2af974854bbee79c299c8ba8
acatzk <joshuaimalay@gmail.com>
2020-06-13 12:14:45 +0800

    Added show-expand in table

--- src/components/pages/InboxTable.vue
+++ src/components/pages/InboxTable.vue
@@ -3,6 +3,7 @@
         :items="items"
         :headers="headers"
         :search="search"
+        show-expand
     >
         <template #item.created_at="{ item }">
             {{ item.created_at.split('T')[0] }}
@@ -16,6 +17,39 @@
             <v-icon left>mail_outline</v-icon>
             {{ item.status }}
         </v-chip>
+        </template>
+        <template #expanded-item="{ headers, item }">
+            <td :colspan="headers.length">
+                <v-row>
+                    <v-col cols="12" md="2">
+                        <b>{{ item.name }}</b>
+                    </v-col>
+                    <v-col cols="12" md="10">
+                        <p>{{ item.message }}</p>
+                    </v-col>
+                </v-row>
+                <v-row class="d-flex justify-center">
+                    <v-btn
+                        class="ma-2 white--text"
+                        color="error"
+                        small
+                        outlined
+                        @click="removedMessage(item)"
+                    >
+                        <v-icon left>delete_outline</v-icon> Remove
+                    </v-btn>
+
+                    <v-btn
+                        small
+                        class="ma-2"
+                        color="success"
+                        outlined
+                        @click="markAsReadMessage(item)"
+                    >
+                        <v-icon left>mail_outline</v-icon> Mark as Read
+                    </v-btn>
+                </v-row>
+            </td>
         </template>
     </v-data-table>
 </template>
@@ -35,6 +69,17 @@ export default {
         },
         search: {
             type: String
+        }
+    },
+
+    methods: {
+        removedMessage(item) {
+            if(confirm('Are you sure you want to delete?')) {
+                alert(item.name)
+            } 
+        },
+        markAsReadMessage(item) {
+            alert(item.message)
         }
     }
 }
