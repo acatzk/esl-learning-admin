105f9140437db9c341487bbcace68a6065fcf35c
acatzk <joshuaimalay@gmail.com>
2020-06-13 10:48:27 +0800

    Displayed data and passed thru props

--- src/components/pages/InboxTable.vue
+++ src/components/pages/InboxTable.vue
@@ -1,3 +1,40 @@
 <template>
-    
+    <v-data-table
+        :items="items"
+        :headers="headers"
+        :search="search"
+    >
+        <template #item.created_at="{ item }">
+            {{ item.created_at.split('T')[0] }}
+        </template>
+        <template #item.status="{ item }">
+        <v-chip
+            :color="item.status === 'Unread' ? 'red' : 'success'"
+            text-color="white"
+        >
+            <v-icon left>mail_outline</v-icon>
+            {{ item.status }}
+        </v-chip>
+        </template>
+    </v-data-table>
 </template>
+
+<script>
+export default {
+    name: 'InboxTable',
+
+    props: {
+        items: {
+            type: Array,
+            required: true
+        },
+        headers: {
+            type: Array,
+            required: true
+        },
+        search: {
+            type: String
+        }
+    }
+}
+</script>
