563883f05825dccd80fc8f917e69dcb8103953c7
acatzk <joshuaimalay@gmail.com>
2020-06-14 11:32:18 +0800

    Chnaged transtion to fade-in-up

--- src/router/index.js
+++ src/router/index.js
@@ -24,7 +24,7 @@ const routes = [
         path: 'inbox/:id',
         name: 'inbox-message',
         component: () => import('@/views/InboxMessage'),
-        meta: { transition: 'flip-y' },
+        meta: { transition: 'fade-in-up' }
       }
     ]
   },
