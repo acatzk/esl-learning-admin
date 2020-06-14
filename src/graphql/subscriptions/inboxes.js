186fa962d099c31975f7ba1eb27d393660491319
acatzk <joshuaimalay@gmail.com>
2020-06-14 15:13:22 +0800

    Added subscription graphql api

--- src/graphql/subscriptions/inboxes.js
+++ src/graphql/subscriptions/inboxes.js
@@ -14,3 +14,17 @@ export const INBOXES_SUBSCRIPTION = gql`
         }
     }
 `
+
+export const _INBOX_SINGLE_SUBSCRIPTION = gql`
+    subscription InboxesSingleSubscription($id: uuid!) {
+        inboxes(order_by: {created_at: desc}, where: {id: {_eq: $id}}) {
+            id
+            name
+            email
+            status
+            contact
+            created_at
+            message
+        }
+    }
+`
