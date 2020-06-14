24ae07fe71b8592b960d6072bf69177df204f5a1
acatzk <joshuaimalay@gmail.com>
2020-06-14 12:54:17 +0800

    Import VueSweetalert2 fiele

--- src/main.js
+++ src/main.js
@@ -5,14 +5,20 @@ import store from "./store";
 import vuetify from "./plugins/vuetify";
 import VuePageTransition from 'vue-page-transition'
 import apolloProvider from './apollo'
+import VueSweetalert2 from 'vue-sweetalert2';
 
 Vue.config.productionTip = false;
 
 /**  VUETIFY ICONS **/
 import 'material-design-icons-iconfont/dist/material-design-icons.css'
 
+/** SWEETALERT2 CSS **/
+import 'sweetalert2/dist/sweetalert2.min.css';
+
 /** VUE-PAGE-TRANSITION **/
 Vue.use(VuePageTransition)
+/** VUE SWEET ALERT 2 **/
+Vue.use(VueSweetalert2);
 
 new Vue({
   router,
