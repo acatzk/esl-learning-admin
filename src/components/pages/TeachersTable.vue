a23ac819ae6d98d110f20c78aa422b809b8b77fc
acatzk <joshuaimalay@gmail.com>
2020-06-15 16:25:45 +0800

    Added options button group fro crud operation

--- src/components/pages/TeachersTable.vue
+++ src/components/pages/TeachersTable.vue
@@ -5,11 +5,59 @@
         :search="search"
     >
         <template #item.is_active="{ item }">
-            <v-chip label :color="item.is_active ? 'green white--text' : 'red white--text'" small>
+            <v-chip label :color="item.is_active ? 'success white--text' : 'error white--text'" small>
                 <v-icon left v-show="item.is_active" style="font-size: 15px;">hourglass_full</v-icon> 
                 <v-icon left v-show="!item.is_active" style="font-size: 15px;">hourglass_empty</v-icon> 
                 {{ item.is_active ? 'Active' : 'Inactive' }}
             </v-chip>
+        </template>
+        <template #item.id="{ item }">  
+            <v-btn-toggle>
+                <v-tooltip top color="primary">
+                    <template v-slot:activator="{ on, attrs }">
+                        <v-btn 
+                            text
+                            v-bind="attrs"
+                            v-on="on"
+                            small 
+                            :class="item.id" 
+                            color="primary"
+                        >
+                        <v-icon>remove_red_eye</v-icon>
+                    </v-btn>
+                    </template>
+                    <span>View</span>
+                </v-tooltip>
+                <v-tooltip top color="warning">
+                    <template v-slot:activator="{ on, attrs }">
+                        <v-btn 
+                            v-bind="attrs"
+                            v-on="on"
+                            small 
+                            text
+                            :class="item.id"
+                            color="warning"
+                        >
+                            <v-icon>colorize</v-icon>
+                        </v-btn>
+                    </template>
+                    <span>Edit</span>
+                </v-tooltip>
+                <v-tooltip top color="error">
+                    <template v-slot:activator="{ on, attrs }">
+                        <v-btn 
+                            v-bind="attrs"
+                            v-on="on"
+                            small
+                            text
+                            color="error"
+                        >
+                            <v-icon>delete_outline</v-icon>
+                        </v-btn>
+                    </template>
+                    <span>Delete</span>
+                </v-tooltip>
+            </v-btn-toggle>
         </template>
     </v-data-table>
 </template>
