13aff29fc4b942884ba303aac756217e91d6e44a
acatzk <joshuaimalay@gmail.com>
2020-06-15 12:27:10 +0800

    Added basic items list and temporary design

---
+++ src/components/pages/TeachersTable.vue
@@ -0,0 +1,34 @@
+<template>
+    <v-data-table
+        :items="items"
+        :headers="headers"
+        :search="search"
+    >
+        <template #item.is_active="{ item }">
+            <v-chip :color="item.is_active ? 'green white--text' : 'red white--text'" small>
+                {{ item.is_active ? 'Active' : 'Inactive' }}
+            </v-chip>
+        </template>
+    </v-data-table>
+</template>
+
+<script>
+export default {
+    name: 'TeachersTable',
+
+    props: {
+        items: {
+            type: Array,
+            required: true
+        },
+        headers: {
+            type: Array,
+            requried: true
+        },
+        search: {
+            type: String,
+            required: true
+        }
+    }
+}
+</script>
