497b55aac914909af20b89aea1540d0d0efc2997
acatzk <joshuaimalay@gmail.com>
2020-06-15 15:09:25 +0800

    Added rounded into search field

--- src/components/layouts/NavBar.vue
+++ src/components/layouts/NavBar.vue
@@ -22,6 +22,7 @@
             <v-text-field
                 solo-inverted
                 flat
+                rounded
                 hide-details
                 label="Search"
                 prepend-inner-icon="search"
