e1ad275f6c079229eb2cba6378a6eb208267ac82
acatzk <joshuaimalay@gmail.com>
2020-06-15 16:39:24 +0800

    Import TeacherActionButton vue component

--- src/components/pages/TeachersTable.vue
+++ src/components/pages/TeachersTable.vue
@@ -12,59 +12,21 @@
             </v-chip>
         </template>
         <template #item.id="{ item }">  
-            <v-btn-toggle>
-                <v-tooltip top color="primary">
-                    <template v-slot:activator="{ on, attrs }">
-                        <v-btn 
-                            text
-                            v-bind="attrs"
-                            v-on="on"
-                            small 
-                            :class="item.id" 
-                            color="primary"
-                        >
-                        <v-icon>remove_red_eye</v-icon>
-                    </v-btn>
-                    </template>
-                    <span>View</span>
-                </v-tooltip>
-                <v-tooltip top color="warning">
-                    <template v-slot:activator="{ on, attrs }">
-                        <v-btn 
-                            v-bind="attrs"
-                            v-on="on"
-                            small 
-                            text
-                            :class="item.id"
-                            color="warning"
-                        >
-                            <v-icon>colorize</v-icon>
-                        </v-btn>
-                    </template>
-                    <span>Edit</span>
-                </v-tooltip>
-                <v-tooltip top color="error">
-                    <template v-slot:activator="{ on, attrs }">
-                        <v-btn 
-                            v-bind="attrs"
-                            v-on="on"
-                            small
-                            text
-                            color="error"
-                        >
-                            <v-icon>delete_outline</v-icon>
-                        </v-btn>
-                    </template>
-                    <span>Delete</span>
-                </v-tooltip>
-            </v-btn-toggle>
+            <teachers-action-button 
+                :item="item"
+            />
         </template>
     </v-data-table>
 </template>
 
 <script>
+
 export default {
     name: 'TeachersTable',
+
+    components: {
+        TeachersActionButton: () => import('./TeachersActionButton')
+    },
 
     props: {
         items: {
