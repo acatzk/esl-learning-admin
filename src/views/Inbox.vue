3dbcd02b271fe24992e5a95f8da7934086863604
acatzk <joshuaimalay@gmail.com>
2020-06-15 12:19:53 +0800

    Removed border in cards

--- src/views/Inbox.vue
+++ src/views/Inbox.vue
@@ -1,40 +1,38 @@
 <template>
   <div class="inbox">
-    <v-container>
-      <v-card outlined>
-        <div class="d-flex justify-space-between">
-            <h3 style="position: relative; top: 20px; left: 20px;">
-                <v-icon>markunread</v-icon> Inbox 
-            </h3>
-            <v-text-field 
-                append-icon="search" 
-                outlined
-                dense
-                style="max-width: 250px; position: relative; top: 14px; right: 10px;"
-                color="deep-purple darken-1"
-                v-model="search"
-              >
-            </v-text-field>
-        </div>
-      </v-card>
-      <v-card 
-        class="mx-auto pa-3" 
-        outlined 
-      >
-          <v-skeleton-loader
-            type="table-tbody"
-            class="mx-auto"
-            tile
-            v-if="$apollo.loading"
-          ></v-skeleton-loader> 
-          <inbox-table 
-            v-else
-            :items="inboxes"
-            :headers="headers"
-            :search="search"
-          />
-      </v-card>
-    </v-container>
+    <v-card flat style="border-bottom: 1px solid #eee;">
+      <div class="d-flex justify-space-between">
+          <h3 style="position: relative; top: 20px; left: 20px;">
+              <v-icon>markunread</v-icon> Inbox 
+          </h3>
+          <v-text-field 
+              append-icon="search" 
+              filled
+              dense
+              style="max-width: 250px; position: relative; top: 14px; right: 10px;"
+              color="deep-purple darken-1"
+              v-model="search"
+            >
+          </v-text-field>
+      </div>
+    </v-card>
+    <v-card 
+      class="mx-auto pa-3" 
+      flat 
+    >
+        <v-skeleton-loader
+          type="table-tbody"
+          class="mx-auto"
+          tile
+          v-if="$apollo.loading"
+        ></v-skeleton-loader> 
+        <inbox-table 
+          v-else
+          :items="inboxes"
+          :headers="headers"
+          :search="search"
+        />
+    </v-card>
   </div>
 </template>
 
