37e7e9eb2a832226524df5519a01c813ca29eb94
acatzk <joshuaimalay@gmail.com>
2020-06-20 13:47:52 +0800

    Added login style email and password

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,5 +1,95 @@
 <template>
-  <div class="login">
-    <h1>Login</h1>
-  </div>
+  <v-app id="inspire">
+    <v-main>
+      <v-container
+        class="fill-height"
+        fluid
+      >
+        <v-row
+          align="center"
+          justify="center"
+        >
+          <v-col
+            cols="12"
+            sm="8"
+            md="4"
+          >
+            <v-card class="elevation-12">
+              <v-toolbar
+                color="deep-purple darken-4"
+                dark
+                flat
+              >
+                <v-toolbar-title>Vic Solution Administrator</v-toolbar-title>
+                <v-spacer></v-spacer>
+                <v-tooltip bottom>
+                  <template v-slot:activator="{ on }">
+                    <v-btn
+                      :href="source"
+                      icon
+                      large
+                      target="_blank"
+                      v-on="on"
+                    >
+                      <v-icon>mdi-web</v-icon>
+                    </v-btn>
+                  </template>
+                  <span>Goto Main Domain</span>
+                </v-tooltip>
+              </v-toolbar>
+              <v-card-text>
+                <v-form>
+                  <v-text-field
+                    label="Email"
+                    prepend-icon="mdi-email-outline"
+                    type="text"
+                  ></v-text-field>
+
+                  <v-text-field
+                    label="Password"
+                    prepend-icon="mdi-lock-outline"
+                    type="password"
+                  ></v-text-field>
+                </v-form>
+              </v-card-text>
+              <v-card-actions>
+                <v-spacer></v-spacer>
+                <v-btn 
+                  outlined 
+                  color="deep-purple darken-4 white--text" 
+                  depressed
+                  text
+                >
+                <v-icon left>mdi-security</v-icon> Login
+                </v-btn>
+              </v-card-actions>
+            </v-card>
+          </v-col>
+        </v-row>
+      </v-container>
+    </v-main>
+  </v-app>
 </template>
+
+<script>
+  export default {
+    name: 'Login',
+
+    data () {
+      return {
+        source: 'https://victorsolutions.netlify.app/'
+      }
+    }
+  }
+</script>
+
+
+<style scoped>
+#inspire {
+  background: #F6F7FB;
+}
+
+.v-btn {
+  text-transform: capitalize;
+}
+</style>
