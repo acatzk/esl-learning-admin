58e129c060de152f598f231205ec9c2908cf68b9
acatzk <joshuaimalay@gmail.com>
2020-06-20 18:36:22 +0800

    Added temporary welcome page text

---
+++ src/views/Welcome.vue
@@ -0,0 +1,8 @@
+<template>
+    <div class="welcome">
+        <h1>Welcome</h1>
+        <v-btn color="indigo white--text" to="/login">
+            Login here
+        </v-btn>
+    </div>
+</template>
