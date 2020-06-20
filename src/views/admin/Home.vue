b753cd1d8adb79f5080cb6e2872d8fe662644c39
acatzk <joshuaimalay@gmail.com>
2020-06-20 17:26:34 +0800

    Removed dashboard component and replace router-view

--- src/views/admin/Home.vue
+++ src/views/admin/Home.vue
@@ -12,13 +12,7 @@
       >
         <vue-page-transition name="fade-in-right" class="content">
 
-          <dashboard-page 
-            class="elevation-5"
-            v-if="$route.path === '/'"
-          /> <!-- ** DASHBOARD PAGE COMPONENT ** -->
-
           <router-view 
-            v-else
             class="elevation-5"
           ></router-view> <!-- ** DYNAMIC CONTENT ROUTE ** -->
 
@@ -45,8 +39,6 @@ export default {
     NavBar: () => import('@/components/layouts/NavBar'),
 
     MainBar: () => import('@/components/layouts/MainBar'),
-
-    DashboardPage: () => import('@/components/pages/DashboardPage'),
 
     FootBar: () => import('@/components/layouts/FootBar')
 
