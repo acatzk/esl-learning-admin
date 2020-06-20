0ef4b9eab5140de2d95725ecbb4b3a6bf4713635
acatzk <joshuaimalay@gmail.com>
2020-06-20 14:52:19 +0800

    Added v-alert vuetify component and created props

---
+++ src/components/pages/Alert.vue
@@ -0,0 +1,34 @@
+<template>
+    <v-alert 
+      :type="type"
+      :border="border"
+      :color="color"
+    >
+      {{ text }}
+    </v-alert>
+</template>
+
+<script>
+export default {
+    name: 'Alert',
+
+    props: {
+        type: {
+            type: String,
+            default: 'error'
+        },
+        border: {
+            type: String,
+            default: 'bottom'
+        },
+        color: {
+            type: String,
+            default: 'pink'
+        },
+        text: {
+            type: String,
+            required: true
+        }
+    }   
+}
+</script>
