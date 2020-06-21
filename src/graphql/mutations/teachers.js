d0ae048e8f7d32a4061ed0249d581defb6cd91f5
acatzk <joshuaimalay@gmail.com>
2020-06-21 13:49:06 +0800

    Modified datebirth to date_birth

--- src/graphql/mutations/teachers.js
+++ src/graphql/mutations/teachers.js
@@ -1,8 +1,8 @@
 import gql from 'graphql-tag'
 
 export const ADD_TEACHER_MUTATION = gql`
-    mutation AddTeacherMutation($firstname: String!, $lastname: String!, $email: String!, $phone: String!, $gender: String!, $birthdate: String!) {
-        insert_teachers(objects: {firstname: $firstname, lastname: $lastname, email: $email, phone: $phone, gender: $gender, birth_date: $birthdate}) {
+    mutation AddTeacherMutation($firstname: String!, $lastname: String!, $email: String!, $phone: String!, $gender: String!, $birth_date: String!) {
+        insert_teachers(objects: {firstname: $firstname, lastname: $lastname, email: $email, phone: $phone, gender: $gender, birth_date: $birth_date}) {
             affected_rows
             returning {
                 id
