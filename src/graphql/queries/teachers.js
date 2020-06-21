02efa98d7e4d570f9a46ce7794e0d1334c4d41da
acatzk <joshuaimalay@gmail.com>
2020-06-21 13:59:25 +0800

    Added api column of profile_url

--- src/graphql/queries/teachers.js
+++ src/graphql/queries/teachers.js
@@ -3,15 +3,16 @@ import gql from 'graphql-tag'
 export const TEACHERS_QUERY = gql`
     query TeachersQuery {
         teachers(order_by: {created_at: desc}) {
-        id
-        firstname
-        lastname
-        email
-        phone
-        gender
-        birth_date
-        is_active
-        created_at
+            id
+            firstname
+            lastname
+            email
+            phone
+            gender
+            birth_date
+            is_active
+            created_at
+            profile_url
         }
     }  
 `
