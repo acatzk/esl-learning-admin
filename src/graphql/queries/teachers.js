ca27597c7589836329f3e888ca7fc9bcb42f39c2
acatzk <joshuaimalay@gmail.com>
2020-06-21 13:14:41 +0800

    Added TEACHERS_QUERY graphql api

---
+++ src/graphql/queries/teachers.js
@@ -0,0 +1,17 @@
+import gql from 'graphql-tag'
+
+export const TEACHERS_QUERY = gql`
+    query TeachersQuery {
+        teachers(order_by: {created_at: desc}) {
+        id
+        firstname
+        lastname
+        email
+        phone
+        gender
+        birth_date
+        is_active
+        created_at
+        }
+    }  
+`
