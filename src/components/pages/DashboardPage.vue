f4471913a2d6ccfcf45ebc456454859f434a1ae1
acatzk <joshuaimalay@gmail.com>
2020-06-21 12:37:17 +0800

    Added cols and romoved margin class

--- src/components/pages/DashboardPage.vue
+++ src/components/pages/DashboardPage.vue
@@ -1,64 +1,68 @@
 <template>
   <div class="elevation-0">
-    <v-row class="mx-3 d-flex justify-space-around">
+    <v-row class="d-flex">
 
-      <v-card
-        class="elevation-8 ml-2 mb-2 card-inbox"
-        width="250"
-      >
-         <v-container class="pa-20">
-            <div class="d-flex justify-space-between align-items-center">
-              <p class="font-weight-normal inbox">Inbox</p>
-              <v-icon size="50">mdi-email-outline</v-icon>
-            </div>
-            <h2 class="font-weight-medium number text-center">291</h2> 
-            <small>Total number of inbox</small>
-         </v-container>
-      </v-card>
+      <v-col cols="12" sm="3">
+        <v-card
+          class="elevation-8 card-inbox"
+        >
+          <v-container>
+              <div class="d-flex justify-space-between align-items-center">
+                <p class="font-weight-normal inbox">Inbox</p>
+                <v-icon size="50">mdi-email-outline</v-icon>
+              </div>
+              <h2 class="font-weight-medium number text-center">291</h2> 
+              <small>Total number of inbox</small>
+          </v-container>
+        </v-card>
+      </v-col>
       
-      <v-card
-        class="elevation-8 ml-2 mb-2 card-lessons"
-        width="250"
-      >
-         <v-container class="pa-20">
-            <div class="d-flex justify-space-between align-items-center">
-              <p class="font-weight-normal inbox">Lessons</p>
-              <v-icon size="50">mdi-folder-multiple-outline</v-icon>
-            </div>
-            <h2 class="font-weight-medium number text-center">35</h2> 
-            <small>Total number of lessons</small>
-         </v-container>
-      </v-card>
+      <v-col cols="12" sm="3">
+        <v-card
+          class="elevation-8 card-lessons"
+        >
+          <v-container class="pa-20">
+              <div class="d-flex justify-space-between align-items-center">
+                <p class="font-weight-normal inbox">Lessons</p>
+                <v-icon size="50">mdi-folder-multiple-outline</v-icon>
+              </div>
+              <h2 class="font-weight-medium number text-center">35</h2> 
+              <small>Total number of lessons</small>
+          </v-container>
+        </v-card>
+      </v-col>
 
 
-       <v-card
-        class="elevation-8 ml-2 mb-2 card-teachers"
-        width="250"
-      >
-         <v-container class="pa-20">
-            <div class="d-flex justify-space-between align-items-center">
-              <p class="font-weight-normal inbox">Teachers</p>
-              <v-icon size="50">mdi-account-multiple-outline</v-icon>
-            </div>
-            <h2 class="font-weight-medium number text-center">225</h2> 
-            <small>Total number of teachers</small>
-         </v-container>
-      </v-card>
+       <v-col cols="12" sm="3">
+         <v-card
+          class="elevation-8 card-teachers"
+        >
+          <v-container class="pa-20">
+              <div class="d-flex justify-space-between align-items-center">
+                <p class="font-weight-normal inbox">Teachers</p>
+                <v-icon size="50">mdi-account-multiple-outline</v-icon>
+              </div>
+              <h2 class="font-weight-medium number text-center">225</h2> 
+              <small>Total number of teachers</small>
+          </v-container>
+        </v-card>
+       </v-col>
 
 
-      <v-card
-        class="elevation-8 ml-2 mb-2 card-students"
-        width="250"
-      >
-         <v-container class="pa-20">
-            <div class="d-flex justify-space-between align-items-center">
-              <p class="font-weight-normal inbox">Students</p>
-              <v-icon size="50">mdi-account-box-outline</v-icon>
-            </div>
-            <h2 class="font-weight-medium number text-center">155</h2> 
-            <small>Total number of teachers</small>
-         </v-container>
-      </v-card>
+      <v-col cols="12" sm="3">
+        <v-card
+          class="elevation-8 card-students"
+        >
+          <v-container class="pa-20">
+              <div class="d-flex justify-space-between align-items-center">
+                <p class="font-weight-normal inbox">Students</p>
+                <v-icon size="50">mdi-account-box-outline</v-icon>
+              </div>
+              <h2 class="font-weight-medium number text-center">155</h2> 
+              <small>Total number of teachers</small>
+          </v-container>
+        </v-card>
+      </v-col>
       
 
     </v-row>
