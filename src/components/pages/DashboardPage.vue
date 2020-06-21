0b08f2691eaa02556ddcced9b989be5e080e7424
acatzk <joshuaimalay@gmail.com>
2020-06-21 12:06:50 +0800

    Added dashboard count style

--- src/components/pages/DashboardPage.vue
+++ src/components/pages/DashboardPage.vue
@@ -1,11 +1,22 @@
 <template>
-  <v-card
-    class="mx-auto"
-    outlined
-    style="height: 75vh;"
-  >
-    
-  </v-card>
+  <div class="elevation-0">
+    <v-row class="mx-3 d-flex justify-space-around">
+      <v-card
+        class="elevation-8 ml-2 mb-2"
+        width="250"
+      >
+         <v-container class="pa-20">
+            <div class="d-flex justify-space-between align-items-center">
+              <p class="font-weight-normal inbox">Inbox</p>
+              <v-icon size="50">mdi-email-outline</v-icon>
+            </div>
+            <h2 class="font-weight-medium number text-center">35</h2> 
+            <small>Total number of inbox</small>
+         </v-container>
+      </v-card>
+      
+    </v-row>
+  </div>
 </template>
 
 <script>
@@ -16,3 +27,21 @@
     },
   }
 </script>
+
+<style scoped>
+.inbox {
+  font-size: 20px;
+}
+
+.number {
+  font-size: 50px;
+}
+
+.total {
+  font-size: 12px;
+}
+
+.v-card {
+  border-bottom: 6px solid #f5365c;
+}
+</style>
