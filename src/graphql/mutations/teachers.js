6073fb367c1b246303d2fdcd2423f3d8d3b49235
acatzk <joshuaimalay@gmail.com>
2020-06-22 12:26:29 +0800

    Added UPDATE_TEACHER_MUTATION graphql api

--- src/graphql/mutations/teachers.js
+++ src/graphql/mutations/teachers.js
@@ -10,3 +10,14 @@ export const ADD_TEACHER_MUTATION = gql`
         }
     }  
 `
+
+export const UPDATE_TEACHER_MUTATION = gql`
+    mutation UpdateTeacherMutation($firstname: String, $lastname: String, $email: String, $phone: String, $gender: String, $birth_date: String, $id: uuid!) {
+        update_teachers(_set: {firstname: $firstname, lastname: $lastname, email: $email, phone: $phone, gender: $gender, birth_date: $birth_date}, where: {id: {_eq: $id}}) {
+            affected_rows
+            returning {
+                id
+            }
+        }
+    }
+`
