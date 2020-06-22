b9c669fccadac152c148eb32b1683c9a97094321
acatzk <joshuaimalay@gmail.com>
2020-06-23 06:52:17 +0800

    Added error ToastAlert in catch when error occur

--- src/components/pages/inboxes/InboxTable.vue
+++ src/components/pages/inboxes/InboxTable.vue
@@ -152,6 +152,24 @@ export default {
                             title: 'Successfully Deleted.'
                         })
                     })
+                    .catch(error => {
+                        const Toast = Swal.mixin({
+                            toast: true,
+                            position: 'top-end',
+                            showConfirmButton: false,
+                            timer: 3000,
+                            timerProgressBar: true,
+                            onOpen: (toast) => {
+                                toast.addEventListener('mouseenter', Swal.stopTimer)
+                                toast.addEventListener('mouseleave', Swal.resumeTimer)
+                            }
+                        })
+
+                        Toast.fire({
+                            icon: 'error',
+                            title: error
+                        })
+                    })
             } 
         },
         markAsReadMessage(item) {
