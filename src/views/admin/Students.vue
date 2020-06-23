05cd0f1847cc6eb9a909d4177ebbb7cf743d0423
acatzk <joshuaimalay@gmail.com>
2020-06-23 18:08:22 +0800

    Added students header page

---
+++ src/views/admin/Students.vue
@@ -0,0 +1,26 @@
+<template>
+    <div class="students">
+       <header class="d-flex justify-space-between">
+            <v-toolbar-title>
+                <v-icon right>mdi-account-box-outline</v-icon> Students List
+            </v-toolbar-title>
+            <v-btn small depressed color="primary" @click.stop="dialog = true">
+               <v-icon left>mdi-plus</v-icon> Add
+            </v-btn>   
+        </header>
+    </div>
+</template>
+
+<style scoped lang="scss">
+.students {
+    header {
+        padding: 15px;
+        border-bottom: 1px solid #eee;
+    }
+    .v-btn-toggle {
+        .v-btn {
+            text-transform: capitalize;
+        }
+    }
+}
+</style>
