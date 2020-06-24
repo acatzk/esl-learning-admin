3dac537034e07c25ca505cde010a5b2dc9d78ddb
acatzk <joshuaimalay@gmail.com>
2020-06-24 11:42:30 +0800

    Added capitalize function for fullname

--- src/components/pages/students/StudentsTable.vue
+++ src/components/pages/students/StudentsTable.vue
@@ -25,6 +25,13 @@ export default {
             type: String,
             required: true
         }
+    },
+
+    methods: {
+        capitalize(s) {
+            if (typeof s !== 'string') return ''
+            return s.charAt(0).toUpperCase() + s.slice(1)
+        }
     }
 
     
