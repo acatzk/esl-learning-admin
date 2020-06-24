f812a8d8c12a9304aa8d0a938436d95eefa29f4e
acatzk <joshuaimalay@gmail.com>
2020-06-24 11:42:04 +0800

    Added v-data-table vuetify component and passed props

---
+++ src/components/pages/students/StudentsTable.vue
@@ -0,0 +1,32 @@
+<template>
+    <v-data-table
+        :items="items"
+        :headers="headers"
+        :search="search"
+    >
+
+    </v-data-table>
+</template>
+
+<script>
+export default {
+    name: 'StudentsTable',
+
+    props: {
+        items: {
+            type: Array,
+            required: true
+        },
+        headers: {
+            type: Array,
+            requried: true
+        },
+        search: {
+            type: String,
+            required: true
+        }
+    }
+
+    
+}
+</script>
