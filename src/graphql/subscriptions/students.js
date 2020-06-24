aaedd836e12ee842f4cbe52d74cc5d8af2960d89
acatzk <joshuaimalay@gmail.com>
2020-06-24 12:42:47 +0800

    Added STUDENT_SUBSCRIPTION graphql api

---
+++ src/graphql/subscriptions/students.js
@@ -0,0 +1,16 @@
+import gql from 'graphql-tag'
+
+export const STUDENT_SUBSCRIPTION = gql`
+    subscription StudentSubscription {
+        students(order_by: {created_at: desc}) {
+            id
+            firstname
+            lastname
+            email
+            contact
+            gender
+            birth_date
+            created_at
+        }
+    }  
+`
