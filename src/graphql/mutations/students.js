12cb0918b1dc7647dcdc735cf5fe33a0836c6739
acatzk <joshuaimalay@gmail.com>
2020-06-25 17:32:58 +0800

    Added ADD_STUDENT_MUTATION graphql api

--- src/graphql/mutations/students.js
+++ src/graphql/mutations/students.js
@@ -10,3 +10,14 @@ export const UPDATE_STUDENT_MUTATION = gql`
         }
     }  
 `
+
+export const ADD_STUDENT_MUTATION = gql`
+    mutation AddStudentMutation($firstname: String!, $lastname: String!, $email: String, $contact: String, $gender: String, $birth_date: String) {
+        insert_students(objects: {firstname: $firstname, lastname: $lastname, email: $email, contact: $contact, gender: $gender, birth_date: $birth_date}) {
+            affected_rows
+            returning {
+                id
+            }
+        }
+    }  
+`
