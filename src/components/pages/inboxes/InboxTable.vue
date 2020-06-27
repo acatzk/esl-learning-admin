7bb541a9d91e23e44849c3a51e9f5abad8345ab4
acatzk <joshuaimalay@gmail.com>
2020-06-27 16:35:51 +0800

    Create toastAlertStatus function for status style

--- src/components/pages/inboxes/InboxTable.vue
+++ src/components/pages/inboxes/InboxTable.vue
@@ -217,46 +217,33 @@ export default {
                 })
                 .then(() => {
                     this.markLoading = false
-                    const Toast = Swal.mixin({
-                        toast: true,
-                        position: 'top-end',
-                        showConfirmButton: false,
-                        timer: 3000,
-                        timerProgressBar: true,
-                        onOpen: (toast) => {
-                            toast.addEventListener('mouseenter', Swal.stopTimer)
-                            toast.addEventListener('mouseleave', Swal.resumeTimer)
-                        }
-                    })
-
-                    Toast.fire({
-                        icon: 'success',
-                        title: 'Marked as Read'
-                    })
+                    this.toastAlertStatus('success', 'Marked as Read')
                 })
                 .catch(error => {
                     this.markLoading = false
-
-                    const Toast = Swal.mixin({
-                        toast: true,
-                        position: 'top-end',
-                        showConfirmButton: false,
-                        timer: 3000,
-                        timerProgressBar: true,
-                        onOpen: (toast) => {
-                            toast.addEventListener('mouseenter', Swal.stopTimer)
-                            toast.addEventListener('mouseleave', Swal.resumeTimer)
-                        }
-                    })
-
-                    Toast.fire({
-                        icon: 'error',
-                        title: error
-                    })
+                    this.toastAlertStatus('error', error)
                 })
         },
         gotoMessage(item) {
             this.$router.push(`/admin/inbox/${item.id}`)
+        },
+        toastAlertStatus(icon, title) {
+            const Toast = Swal.mixin({
+                toast: true,
+                position: 'top-end',
+                showConfirmButton: false,
+                timer: 3000,
+                timerProgressBar: true,
+                onOpen: (toast) => {
+                    toast.addEventListener('mouseenter', Swal.stopTimer)
+                    toast.addEventListener('mouseleave', Swal.resumeTimer)
+                }
+            })
+
+            Toast.fire({
+                icon: icon,
+                title: title
+            })
         }
     }
 }
