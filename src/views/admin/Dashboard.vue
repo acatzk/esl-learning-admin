1a6a0127e82464cdee402d48af2d0910ea938c73
acatzk <joshuaimalay@gmail.com>
2020-06-28 09:40:43 +0800

    Added dummy graph in dashboard

--- src/views/admin/Dashboard.vue
+++ src/views/admin/Dashboard.vue
@@ -66,22 +66,67 @@
 
     <v-card 
       class="mx-auto visitor elevation-1" 
-      style="height: 45vh;">
+    >
+      <v-sheet
+        class="v-sheet--offset mx-auto"
+        color="indigo lighten-1"
+        elevation="0"
+      >
+        <v-sparkline
+          :labels="labels"
+          :value="value"
+          color="white"
+          line-width="2"
+          padding="16"
+        ></v-sparkline>
+      </v-sheet>
 
+      <v-card-text class="pt-0">
+        <div class="title font-weight-light mb-2">User Registrations</div>
+        <div class="subheading font-weight-light grey--text">Last Campaign Performance</div>
+        <v-divider class="my-2"></v-divider>
+        <v-icon
+          class="mr-2"
+          small
+        >
+          mdi-clock
+        </v-icon>
+        <span class="caption grey--text font-weight-light">last registration 26 minutes ago</span>
+      </v-card-text>
     </v-card>
   </div>
 </template>
 
 <script>
   export default {
-    data () {
-      return {
-      }
-    },
+    data: () => ({
+      labels: [
+        '12am',
+        '3am',
+        '6am',
+        '9am',
+        '12pm',
+        '3pm',
+        '6pm',
+        '9pm',
+      ],
+      value: [
+        200,
+        675,
+        410,
+        390,
+        310,
+        460,
+        250,
+        240,
+      ],
+    }),
   }
 </script>
 
+
 <style scoped>
+
 .inbox {
   font-size: 20px;
 }
