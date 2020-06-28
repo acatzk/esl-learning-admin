293ae5c63d9f9cc9530a541b88149951e452cd3f
acatzk <joshuaimalay@gmail.com>
2020-06-28 09:20:10 +0800

    Removed v-row and added comment

--- src/views/admin/Profile.vue
+++ src/views/admin/Profile.vue
@@ -23,80 +23,9 @@
                                <v-icon left>mdi-pencil</v-icon> Edit Profile
                             </v-btn>
                        </header>
-                        <v-row class="ma-3">
-                            <v-col cols="12" sm="6" md="4">
-                                <v-text-field 
-                                    label="Firstname" 
-                                    autocomplete="off"
-                                    prepend-inner-icon="mdi-account-outline"
-                                ></v-text-field>
-                            </v-col>
-                            <v-col cols="12" sm="6" md="4">
-                                <v-text-field 
-                                    label="Middlename" 
-                                    autocomplete="off" 
-                                    prepend-inner-icon="mdi-account-outline"
-                                ></v-text-field>
-                            </v-col>
-                            <v-col cols="12" sm="6" md="4">
-                                <v-text-field 
-                                    label="Lastname" 
-                                    autocomplete="off"
-                                    prepend-inner-icon="mdi-account-outline"
-                                ></v-text-field>
-                            </v-col>
-                            <v-col cols="12" sm="6">
-                                <v-text-field 
-                                    label="Company" 
-                                    autocomplete="off"
-                                    prepend-inner-icon="mdi-hospital-building"
-                                ></v-text-field>
-                            </v-col>
-                            <v-col cols="12" sm="6">
-                                <v-text-field 
-                                    label="Location" 
-                                    autocomplete="off"
-                                    prepend-inner-icon="mdi-map-marker-circle"
-                                ></v-text-field>
-                            </v-col>
-                            <v-col cols="12" sm="6">
-                                <v-text-field 
-                                    label="Website" 
-                                    autocomplete="off"
-                                    prepend-inner-icon="mdi-link"
-                                ></v-text-field>
-                            </v-col>
-                             <v-col cols="12" sm="6">
-                                <v-text-field 
-                                    label="Twitter/username" 
-                                    autocomplete="off"
-                                    prepend-inner-icon="mdi-twitter"
-                                ></v-text-field>
-                            </v-col>
-                            <v-col cols="12">
-                                <v-textarea
-                                    clearable
-                                    clear-icon="mdi-close"
-                                    label="Bio"
-                                    autocomplete="off"
-                                    prepend-inner-icon="mdi-paper-cut-vertical"
-                                    rows="1"
-                                ></v-textarea>
-                            </v-col>
-                            <v-col cols="12">
-                                <v-btn  
-                                    color="success"
-                                    class="float-right"
-                                >
-                                    Save
-                                </v-btn>
-                                <v-btn  
-                                    class="float-right mr-2"
-                                >
-                                    Cancel
-                                </v-btn>
-                            </v-col>
-                        </v-row>
+                        
+                        <edit-profile /> <!-- Editable Profile view -->
+                        
                     </v-container>
                 </v-card>
             </v-col>
@@ -127,6 +56,15 @@
     </div>
 </template>
 
+<script>
+export default {
+    name: 'Profile',
+
+    components: {
+        EditProfile: () => import('@/components/pages/profile/EditProfile')
+    }
+}
+</script>
 
 <style scoped lang="scss">
 
