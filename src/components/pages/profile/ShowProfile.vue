9dc2cd72d6278aedbd089bdaeb8f7b08ae1706c6
acatzk <joshuaimalay@gmail.com>
2020-06-28 11:10:18 +0800

    Added v-chip in firstname field

---
+++ src/components/pages/profile/ShowProfile.vue
@@ -0,0 +1,18 @@
+<template>
+    <v-row class="ma-3">
+        <v-col cols="12" sm="6" md="4">
+            <v-chip
+                class="ma-2"
+            >
+            <v-avatar left>
+                <v-icon>mdi-account-outline</v-icon>
+            </v-avatar>
+                Firstname...
+            </v-chip>
+        </v-col>
+    </v-row>
+</template>
+
+<style scoped>
+
+</style>
