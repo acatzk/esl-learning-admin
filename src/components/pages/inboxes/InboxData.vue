461e51cf597410aeed67ddf0da0f6e701ba9533b
acatzk <joshuaimalay@gmail.com>
2020-06-29 11:58:15 +0800

    Removed the data in inboxes and make into dynamic to match the route

---
+++ src/components/pages/inboxes/InboxData.vue
@@ -0,0 +1,86 @@
+<template>
+    <div class="inbox-data">
+        <!-- ** SEARCH TEXT FIELD ** -->
+        <v-text-field
+            label="Search"
+            v-model="search"
+            filled
+            class="mx-3"
+            append-icon="search"
+            dense
+            rounded
+            style="position: relative; top: 10px; border-radius: 50px;"
+        ></v-text-field>
+
+        <!-- ** SKELETON LOADING FOR DATA ** -->
+        <v-skeleton-loader
+            type="table-tbody"
+            class="mx-auto"
+            tile
+            v-if="$apollo.loading"
+        ></v-skeleton-loader> 
+
+
+        <!-- ** TEACHERS VUETIFY TABLE ** -->
+        <inbox-table 
+            v-else
+            :items="inboxes"
+            :headers="headers"
+            :search="search"
+        />
+    </div>
+</template>
+
+<script>
+
+// INBOXES QUERY 
+import { INBOX_QUERY } from '@/graphql/queries/inboxes'
+// INBOXES SUBSCRIPTION
+import { INBOXES_SUBSCRIPTION } from '@/graphql/subscriptions/inboxes'
+
+export default {
+    name: 'InboxData',
+
+    components: {
+        InboxTable: () => import('@/components/pages/inboxes/InboxTable')
+    },
+
+    data: () => ({
+        inboxes: [],
+        search: ''
+    }),
+
+    computed: {
+        headers() {
+        return [
+            { text: 'Name',  value: 'name' },
+            { text: 'Email', value: 'email' },
+            { text: 'Date', value: 'created_at' },
+            { text: 'Contact', value: 'contact', sortable: false },
+            { text: 'Status', value: 'status', sortable: false }
+        ]
+        }
+    },
+
+    apollo: {
+        inboxes: {
+            query: INBOX_QUERY,
+            subscribeToMore: {
+                document: INBOXES_SUBSCRIPTION,
+                updateQuery(previousResult, { subscriptionData }) {
+                if (previousResult) {
+                    return {
+                    inboxes: [
+                        ...subscriptionData.data.inboxes
+                    ]
+                    }
+                }
+                }
+            },
+            result ({ data }) {
+                this.inboxes = data.inboxes
+            }
+        }
+    }
+}
+</script>
