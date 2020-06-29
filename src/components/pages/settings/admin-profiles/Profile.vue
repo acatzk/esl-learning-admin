215f0592cb40fc8304dc7b4875c2495bd7bb4663
acatzk <joshuaimalay@gmail.com>
2020-06-30 07:39:17 +0800

    Setup Headers and text search design

--- src/components/pages/settings/admin-profiles/Profile.vue
+++ src/components/pages/settings/admin-profiles/Profile.vue
@@ -1,17 +1,43 @@
 <template>
     <div class="profile-setting">
-        <v-card
-            style="height: 80vh;"
-            class="elevation-1"
-        >
-            <h1>Profile Settings</h1>
+        <header class="d-flex justify-space-between">
+            <v-toolbar-title>
+                <v-icon right>mdi-account-box</v-icon> Administrator Settings
+            </v-toolbar-title>
+            <v-btn small depressed color="primary" @click.stop="dialog = true">
+               <v-icon left>mdi-plus</v-icon> Add
+            </v-btn>   
+        </header>
 
+        <v-card flat>
+            <!-- ** SEARCH TEXT FIELD ** -->
+            <v-text-field
+                label="Search"
+                filled
+                class="mx-3"
+                append-icon="search"
+                dense
+                rounded
+                style="position: relative; top: 12px; border-radius: 50px;"
+            ></v-text-field>
         </v-card>
     </div>
 </template>
 
-<style scoped>
+<style scoped lang="scss">
 .profile-setting {
-    background: transparent;
+    background: white;
+    border: 1px solid #eee;
+}
+.profile-setting {
+    header {
+        padding: 15px;
+        border-bottom: 1px solid #eee;
+    }
+    .v-btn-toggle {
+        .v-btn {
+            text-transform: capitalize;
+        }
+    }
 }
 </style>
