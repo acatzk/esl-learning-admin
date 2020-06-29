fb07f7af3dc4c37d9b2e5621eb8fac84d2b84933
acatzk <joshuaimalay@gmail.com>
2020-06-29 08:19:58 +0800

    Added minLength validation rule

--- src/assets/js/validationRules.js
+++ src/assets/js/validationRules.js
@@ -6,3 +6,7 @@ export const emailRules = (propertyType) => {
     return v => /^\w+([.-]?\w+)*@\w+([.-]?\w+)*(\.\w{2,3})+$/.test(v) || `${propertyType} address must be valid.`
 }
 
+export const minLength = (propertyType, minLength) => {
+    return v => v && v.length >= minLength || `${propertyType} must be at least ${minLength} characters.`
+}
+
