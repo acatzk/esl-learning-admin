2ebee27ab2a7fe6f4db6921e0b9239ac94b85763
acatzk <joshuaimalay@gmail.com>
2020-06-29 10:09:26 +0800

    Added comment in components

--- src/views/admin/Settings.vue
+++ src/views/admin/Settings.vue
@@ -3,68 +3,30 @@
         <v-row>
             <v-col cols="12" sm="9">
                 <v-card
-                    style="height: 80vh; border: 1px solid #eee;"
-                    class="elevation-0"
-                >
-                    
-                </v-card>
-            </v-col>
-            <v-col cols="12" sm="3">
-                <v-card
                     style="height: 80vh;"
                     class="elevation-1"
                 >
-                    <template>
-                        <v-list-item two-line>
-                            <v-list-item-avatar>
-                                <img src="https://avatars0.githubusercontent.com/u/38458781?s=400&u=d2e21173ef861da3d8ca3a292398cbc0c6f7c000&v=4">
-                            </v-list-item-avatar>
 
-                            <v-list-item-content>
-                                <v-list-item-title class="font-weight-medium">Joshua Galit</v-list-item-title>
-                                <small class="small">Personal settings</small>
-                            </v-list-item-content>
-                        </v-list-item>
-                    </template>
+                    <router-view></router-view> <!-- Dynamic setting view -->
 
-                    <v-divider></v-divider>
-                    <v-list dense>
-                        <v-list-item
-                            v-for="item in items"
-                            :key="item.title"
-                            :to="item.to"
-                        >
-                            <v-list-item-icon>
-                                <v-icon>{{ item.icon }}</v-icon>
-                            </v-list-item-icon>
-
-                            <v-list-item-content>
-                                <v-list-item-title>{{ item.title }}</v-list-item-title>
-                            </v-list-item-content>
-                        </v-list-item>
-                    </v-list>
                 </v-card>
+            </v-col>
+            <v-col cols="12" sm="3">
+
+                <side-bar /> <!-- SideBar vue component -->
+
             </v-col>
         </v-row>
     </div>
 </template>
 
+
 <script>
-  export default {
-    data () {
-      return {
-        items: [
-          { title: 'Profile', icon: 'mdi-tag-faces', to: '/admin/settings' },
-          { title: 'Account', icon: 'mdi-account-circle', to: '/admin/settings/account' },
-          { title: 'Teachers', icon: 'mdi-folder-account', to: '/admin/settings/teachers' },
-          { title: 'Security', icon: 'mdi-security-network', to: '/admin/settings/security' },
-          { title: 'Security log', icon: 'mdi-key-variant', to: '/admin/settings/security-log' },
-          { title: 'Emails', icon: 'mdi-email-open', to: '/admin/settings/emails' },
-          { title: 'Notifications', icon: 'mdi-bell-ring', to: '/admin/settings/notications' },
-          { title: 'Billings', icon: 'mdi-deskphone', to: '/admin/settings/billings' },
-          { title: 'Developers Info', icon: 'mdi-code-tags', to: '/admin/settings/developers-info' },
-        ],
-      }
-    },
-  }
+export default {
+    name: 'Settings',
+
+    components: {
+        SideBar: () => import('@/components/pages/settings/SideBar')
+    }
+}
 </script>
