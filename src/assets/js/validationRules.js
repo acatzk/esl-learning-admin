3765de001b5da0841c34fb9a8507babc9a84006d
acatzk <joshuaimalay@gmail.com>
2020-06-29 08:18:57 +0800

    Added emailRules validation rules

--- src/assets/js/validationRules.js
+++ src/assets/js/validationRules.js
@@ -2,3 +2,7 @@ export const required = (propertyType) => {
     return v => v && v.length > 0 || `${propertyType} is required.`
 }
 
+export const emailRules = (propertyType) => {
+    return v => /^\w+([.-]?\w+)*@\w+([.-]?\w+)*(\.\w{2,3})+$/.test(v) || `${propertyType} address must be valid.`
+}
+
