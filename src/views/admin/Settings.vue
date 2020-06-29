190269d356419814e0e8ddb7b07c8d1687a589f9
acatzk <joshuaimalay@gmail.com>
2020-06-30 07:11:57 +0800

    Removed v-card

--- src/views/admin/Settings.vue
+++ src/views/admin/Settings.vue
@@ -2,14 +2,9 @@
     <div class="elevation-0">
         <v-row>
             <v-col cols="12" sm="9">
-                <v-card
-                    style="height: 80vh;"
-                    class="elevation-1"
-                >
 
-                    <router-view></router-view> <!-- Dynamic setting view -->
+                <router-view></router-view> <!-- Dynamic setting view -->
 
-                </v-card>
             </v-col>
             <v-col cols="12" sm="3">
 
