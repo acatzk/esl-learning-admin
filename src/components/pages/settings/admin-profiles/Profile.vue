077aaa4d110e45ac230f0210dc434cff0f456655
acatzk <joshuaimalay@gmail.com>
2020-06-30 07:17:59 +0800

    Moved Profile vue component inside the admin-profiles folder

---
+++ src/components/pages/settings/admin-profiles/Profile.vue
@@ -0,0 +1,17 @@
+<template>
+    <div class="profile-setting">
+        <v-card
+            style="height: 80vh;"
+            class="elevation-1"
+        >
+            <h1>Profile Settings</h1>
+
+        </v-card>
+    </div>
+</template>
+
+<style scoped>
+.profile-setting {
+    background: transparent;
+}
+</style>
