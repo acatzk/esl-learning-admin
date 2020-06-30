3a2a91e9ab9bc7af4d31b8d8aa274de67f6e2951
acatzk <joshuaimalay@gmail.com>
2020-06-30 17:05:43 +0800

    Set up dummy aggregate count set to 0

---
+++ src/components/pages/dashboard/LessonsCount.vue
@@ -0,0 +1,30 @@
+<template>
+    <v-container>
+        <div class="d-flex justify-space-between align-items-center">
+        <p class="font-weight-normal" style="font-size: 20px;">Lessons</p>
+        <v-icon size="50">mdi-folder-multiple-outline</v-icon>
+        </div>
+        <h2 
+            class="font-weight-medium text-center" 
+            style="font-size: 50px;"
+        >
+            {{ lessonsCount ? lessonsCount.aggregate.count : 0 }}
+        </h2> 
+        <small>Total number of lessons</small>
+    </v-container>
+</template>
+
+
+<script>
+
+export default {
+    name: 'LessonsCount',
+
+    data () {
+        return {
+            lessonsCount: 0
+        }
+    },
+
+}
+</script>
