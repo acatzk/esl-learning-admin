a3bb1afd8bf0a86a109ebcde54c8eb0603b19826
acatzk <joshuaimalay@gmail.com>
2020-06-30 17:25:43 +0800

    Added realtime TeachersCount total

---
+++ src/components/pages/dashboard/TeachersCount.vue
@@ -0,0 +1,53 @@
+<template>
+    <v-container>
+        <div class="d-flex justify-space-between align-items-center">
+        <p class="font-weight-normal" style="font-size: 20px;">Teachers</p>
+        <v-icon size="50">mdi-account-multiple-outline</v-icon>
+        </div>
+        <h2 
+            class="font-weight-medium text-center" 
+            style="font-size: 50px;"
+        >
+            {{ teacherCount ? teacherCount.aggregate.count : 0 }}
+        </h2> 
+        <small>Total number of teachers</small>
+    </v-container>
+</template>
+
+<script>
+
+import { TOTAL_TEACHERS_COUNT_QUERY } from '@/graphql/queries/teachers'
+
+import { TOTAL_TEACHERS_COUNT_SUBSCRIPTION } from '@/graphql/subscriptions/teachers'
+
+export default {
+    name: 'TeachersCount',
+
+    data () {
+        return {
+            teacherCount: 0
+        }
+    },
+
+    apollo: {
+      teacherCount: {
+        query: TOTAL_TEACHERS_COUNT_QUERY,
+        subscribeToMore: {
+          document: TOTAL_TEACHERS_COUNT_SUBSCRIPTION,
+          updateQuery(previousResult, { subscriptionData }) {
+            if (previousResult) {
+              return {
+                teacherCount: {
+                  ...subscriptionData.data.teacherCount
+                }
+              }
+            }
+          }
+        },
+        result ({ data }) {
+          this.teacherCount = data.teacherCount
+        }
+      }
+    }
+}
+</script>
