0fa792e29f418ace263363570941341bc65d2353
acatzk <joshuaimalay@gmail.com>
2020-07-02 15:42:13 +0800

    Create TabSettings vue component and added tab value

--- src/components/pages/settings/Account.vue
+++ src/components/pages/settings/Account.vue
@@ -1,13 +1,14 @@
 <template>
-    <div class="account-setting">
-        <v-card style="height: 70vh;" class="elevation-0">
-
-        </v-card>
+    <div class="account-setting elevation-0 pa-3">
+        <v-toolbar-title class="mb-2">
+            <h3 class="font-weight-medium">Account Settings</h3>
+        </v-toolbar-title>
+        <v-divider></v-divider>
     </div>
 </template>
 
 <style scoped>
-.account-setting {
-    background: white;
+h3 {
+    color: rgb(48, 1, 82);
 }
 </style>
---
+++ src/components/pages/settings/TabSettings.vue
@@ -0,0 +1,81 @@
+<template>
+    <div class="tab-settings">
+        <v-tabs horizontal fixed-tabs color="indigo">
+            <v-tab>
+                <v-icon left>mdi-account-circle</v-icon>
+                Account
+            </v-tab>
+            <v-tab>
+                <v-icon left>settings</v-icon>
+                Administrators
+            </v-tab>
+            <v-tab>
+                <v-icon left>mdi-folder-account</v-icon>
+                Teachers
+            </v-tab>
+            <v-tab>
+                <v-icon left>mdi-code-tags</v-icon>
+                Developers Info
+            </v-tab>
+
+            <v-tab-item>
+                <v-card flat>
+                    <v-card-text>
+                        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Perferendis sed, temporibus reiciendis dolorum voluptatibus cum vero sunt assumenda quaerat tempore velit alias quos rem, vitae perspiciatis, facilis aliquam. Excepturi laboriosam dolorum sapiente rerum est voluptates consectetur impedit porro corporis! A, reprehenderit dolorum nihil ratione autem officia hic ex suscipit sint ea earum consectetur, eaque minima at delectus dolores. Officiis pariatur sunt possimus natus dolorum. Perferendis distinctio maiores dignissimos velit laudantium obcaecati ex optio possimus similique est voluptates consectetur facere corrupti earum laboriosam harum, cupiditate temporibus debitis in eum modi officiis eius! Minima magnam cupiditate fugiat reiciendis quaerat at repellat tenetur nostrum voluptas voluptatibus consectetur asperiores sed consequuntur mollitia culpa, sit accusantium impedit fuga ab recusandae debitis sequi exercitationem? Cum deleniti magni, id asperiores quae perspiciatis sunt laboriosam reprehenderit porro qui provident, beatae quaerat repudiandae deserunt et in nostrum amet optio fugit aperiam. In laborum earum incidunt neque, vero dolores eos optio quia ex consequatur laboriosam tenetur ratione blanditiis iure quae corrupti eius consectetur nemo aut quam. Voluptatibus dolore aspernatur accusamus dolorum quisquam eaque ratione a temporibus deleniti sint, asperiores reprehenderit soluta velit sequi vitae quas delectus labore aliquam eveniet maxime veniam reiciendis enim, repellat quidem! Dolore ipsum alias libero officia reiciendis tempora ipsa, earum error esse pariatur exercitationem quos ex maiores qui numquam? Repellat aperiam quaerat consequatur architecto. Ex quam harum commodi mollitia esse possimus suscipit, nam nesciunt? Sit, itaque at laborum, libero maxime reiciendis accusantium repellendus sapiente debitis dignissimos exercitationem nulla consequuntur praesentium! Repellendus, tempore impedit. Rem ut doloremque hic harum natus quae quas, illo molestias temporibus ratione autem magni quasi exercitationem at quibusdam blanditiis id nobis. Porro aut illo itaque delectus eos quaerat atque, modi, deleniti ipsum, inventore similique necessitatibus dolor alias id officiis nobis est reiciendis esse corporis saepe quae minima quod suscipit. Voluptatibus ea odio molestiae.
+                    </v-card-text>
+                </v-card>
+            </v-tab-item>
+            <v-tab-item>
+                <Administrator /> <!-- ADMINISTRATOR SETTINGS -->
+            </v-tab-item>
+            <v-tab-item>
+                <v-card flat>
+                    <v-card-text>
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Quibusdam, perferendis cupiditate quas illum error natus suscipit ut non mollitia hic ratione distinctio quaerat eligendi, dignissimos laborum, eaque voluptate quo eveniet deserunt? Magni adipisci harum, voluptate aut necessitatibus sed molestias. Quam velit laboriosam provident magnam ea tempore nesciunt dicta optio expedita ipsam at cum excepturi sed aut numquam dolore, eius accusamus incidunt quae magni repellendus, eligendi explicabo! Temporibus dolorum architecto delectus ullam laborum. Blanditiis aliquid autem iusto fugit voluptas, ab, et, placeat ad voluptates ipsum neque. Rem eos ex veniam quisquam fuga quod possimus sed soluta consequuntur! Cumque eaque voluptate quod pariatur voluptates vitae ratione ipsam unde alias! Atque quo veritatis aliquam perferendis aspernatur accusantium tempora consequatur dolor velit dolorum officiis debitis harum sit quidem, temporibus molestiae dignissimos totam molestias dolores libero doloribus impedit eveniet alias nam. Placeat odit explicabo consequatur sunt. Nisi eveniet impedit libero, doloremque explicabo esse modi perspiciatis odit expedita, exercitationem, nam amet laborum? Et nostrum itaque dolorem maiores distinctio optio ex vero neque, quos perferendis illum ullam voluptatum, consequatur modi quod natus? Laboriosam exercitationem facere, nihil quod reiciendis fugiat veritatis doloribus quibusdam enim possimus maiores voluptatibus! Veniam, molestias vel dolorum modi deleniti illo dolore officiis, necessitatibus vero suscipit omnis voluptatibus nam odio? Numquam pariatur sequi repellendus necessitatibus distinctio iure esse autem architecto ratione quam earum deserunt alias, maiores consequuntur magni sapiente, est quia exercitationem suscipit voluptas libero voluptatum ipsum quasi. Dolores ducimus consequuntur repellat maiores a mollitia, aspernatur, sit, aut impedit ullam temporibus. Fugit maxime adipisci, eveniet molestiae iste ut recusandae repudiandae deleniti nam, voluptates alias consectetur esse incidunt animi. Ad aut temporibus at nam beatae praesentium repellat vitae ex quaerat minus harum adipisci minima maiores facilis quasi impedit, voluptas doloremque saepe delectus. Labore fuga doloribus hic dolore neque! Sapiente, fugiat quas fuga possimus dolorum itaque repellendus?
+                    </v-card-text>
+                </v-card>
+            </v-tab-item>
+            <v-tab-item>
+                <v-card flat>
+                    <v-card-text>
+                        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Error tempore, a molestiae repellat illum, amet odit nisi nam totam, ut voluptatem vel sunt dicta soluta! Similique earum deleniti voluptatem quaerat ea, facere repellat consequatur id sunt nostrum. Aliquid eum voluptatibus, nam non ducimus distinctio consequatur suscipit at porro inventore est tempore cumque sequi dolorem enim eius libero quae atque. Assumenda veniam excepturi consequatur earum quas, molestias perspiciatis velit eum rem facilis. Excepturi dolor assumenda illum voluptatem libero veritatis aliquam ipsam quae. Recusandae incidunt eius quisquam hic, fuga deleniti nam quae magnam enim molestiae nulla velit, nostrum et eaque doloribus a! Quisquam, dolor. Praesentium ipsa numquam provident nisi tempore porro recusandae doloribus cum voluptatem eos vitae ducimus perferendis, voluptatibus natus esse molestiae exercitationem sunt sed expedita soluta odit inventore delectus corporis ea! Repellat quos reiciendis molestias nihil dolor vero at deserunt maxime nostrum doloremque! Aperiam cupiditate voluptate temporibus minus beatae, adipisci laborum eligendi rerum! Fugiat pariatur explicabo magni ducimus sequi reprehenderit debitis harum. Molestiae debitis pariatur velit aut culpa quos, impedit facilis expedita doloremque exercitationem consequuntur ad optio quas laboriosam possimus delectus repudiandae doloribus temporibus nihil placeat asperiores aliquid hic quis! Quam vel voluptatem, quasi sequi dicta accusamus commodi obcaecati id optio similique aliquid recusandae voluptas placeat consequuntur minima dolorum corporis voluptatum. Illo tenetur illum magni expedita ullam doloribus iusto praesentium, error eveniet veniam, voluptates assumenda nemo ut nisi unde, similique sed quas modi. Culpa voluptas voluptate magni excepturi dolore, hic exercitationem repudiandae ratione laudantium dolores minima repellendus ad perspiciatis vero ut! Quibusdam dignissimos ipsam quas est a dicta nam necessitatibus et voluptates magnam fugit doloribus, mollitia velit nisi ratione eligendi molestias architecto cumque. A cupiditate soluta sapiente culpa eaque veniam delectus, enim rem magni nam molestiae dicta, in doloremque nostrum consequuntur architecto ipsam asperiores provident. Expedita obcaecati quidem impedit architecto!
+                    </v-card-text>
+                </v-card>
+            </v-tab-item>
+        </v-tabs>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'TabSettings',
+
+    data () {
+        return {
+            search: '',
+        }
+    },
+
+    components: {
+        Administrator: () => import('./admin-profiles/Admin')
+    }
+}
+</script>
+
+<style scoped>
+
+header {
+  padding: 15px;
+  border-bottom: 1px solid #eee;
+}
+
+.v-tabs {
+    border: none;
+    box-shadow: none;
+}
+
+.v-tab {
+    text-transform: capitalize;
+}
+
+</style>
