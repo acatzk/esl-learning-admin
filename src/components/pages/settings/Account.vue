13308d31cb6dd4d4edb5fb25f2ae4afc4041bb9d
acatzk <joshuaimalay@gmail.com>
2020-07-02 16:36:30 +0800

    Added field for account update

--- src/components/pages/settings/Account.vue
+++ src/components/pages/settings/Account.vue
@@ -1,14 +1,46 @@
 <template>
-    <div class="account-setting elevation-0 pa-3">
-        <v-toolbar-title class="mb-2">
-            <h3 class="font-weight-medium">Account Settings</h3>
-        </v-toolbar-title>
-        <v-divider></v-divider>
+    <div class="account-setting">
+        <v-container>
+            <form>
+                <v-toolbar-title class="text-center ma-3 font-weight-medium grey--text">
+                    Firebase Account Authentication
+                </v-toolbar-title>
+                <v-text-field
+                    label="E-mail"
+                    filled
+                    v-model="email"
+                    rounded
+                ></v-text-field>
+                <v-text-field
+                    label="Password"
+                    v-model="password"
+                    filled
+                    rounded
+                ></v-text-field>
+                <v-btn class="mr-4" color="indigo white--text">
+                   <v-icon left>mdi-pencil</v-icon> UPDATE</v-btn>
+            </form>
+        </v-container>
     </div>
 </template>
 
+
+<script>
+export default {
+    name: 'Account',
+
+    data () {
+        return {
+            email: 'joshuaimalay@gmail.com',
+            password: '1pLFxjNITScCFXRF9j3s7llf92j2'
+        }
+    }
+
+}
+</script>
+
 <style scoped>
-h3 {
-    color: rgb(48, 1, 82);
+.account-setting {
+    border-top: 1px solid #eee;
 }
 </style>
