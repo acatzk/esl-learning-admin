a96e40e85524ee6b869fddd312391cda6414a37a
acatzk <joshuaimalay@gmail.com>
2020-07-05 08:27:25 +0800

    Get all account query in accounts administrator graphql api

--- src/graphql/queries/accounts.js
+++ src/graphql/queries/accounts.js
@@ -10,3 +10,14 @@ export const ACCOUNT_QUERY = gql`
         }
     }  
 `
+
+export const ALL_ACCOUNT_QUERY = gql`
+    query AccountQueries {
+        accounts(order_by: {created_at: desc}) {
+            id
+            email
+            password
+            created_at
+        }
+    }  
+`
