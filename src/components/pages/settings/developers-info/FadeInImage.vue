ed9cf07c0072e1e302aea20e5d424b648f99d8f3
acatzk <joshuaimalay@gmail.com>
2020-07-08 07:56:43 +0800

    Added FadeIn funtionanlity

---
+++ src/components/pages/settings/developers-info/FadeInImage.vue
@@ -0,0 +1,17 @@
+<template functional>
+  <v-lazy-image
+    :src="props.src"
+    :src-placeholder="props.srcPlaceholder"
+    alt="Periscope"
+  ></v-lazy-image>
+</template>
+
+<style scoped>
+.v-lazy-image {
+  opacity: 0;
+  transition: opacity 2s;
+}
+.v-lazy-image-loaded {
+  opacity: 1;
+}
+</style>
