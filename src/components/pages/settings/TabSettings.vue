366fd03a1921a18e06db7279d91b451ae5383b60
acatzk <joshuaimalay@gmail.com>
2020-07-07 15:34:49 +0800

    Imort TeachersAccount vue component

--- src/components/pages/settings/TabSettings.vue
+++ src/components/pages/settings/TabSettings.vue
@@ -19,20 +19,24 @@
             </v-tab>
 
             <v-tab-item>
+
                 <account /> <!-- ACCOUNT SETTINGS -->
+
             </v-tab-item>
             <v-tab-item>
-                <Administrator /> <!-- ADMINISTRATOR SETTINGS -->
+
+                <administrator /> <!-- ADMINISTRATOR SETTINGS -->
+
             </v-tab-item>
             <v-tab-item>
-                <v-card flat>
-                    <v-card-text>
-                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Quibusdam, perferendis cupiditate quas illum error natus suscipit ut non mollitia hic ratione distinctio quaerat eligendi, dignissimos laborum, eaque voluptate quo eveniet deserunt? Magni adipisci harum, voluptate aut necessitatibus sed molestias. Quam velit laboriosam provident magnam ea tempore nesciunt dicta optio expedita ipsam at cum excepturi sed aut numquam dolore, eius accusamus incidunt quae magni repellendus, eligendi explicabo! Temporibus dolorum architecto delectus ullam laborum. Blanditiis aliquid autem iusto fugit voluptas, ab, et, placeat ad voluptates ipsum neque. Rem eos ex veniam quisquam fuga quod possimus sed soluta consequuntur! Cumque eaque voluptate quod pariatur voluptates vitae ratione ipsam unde alias! Atque quo veritatis aliquam perferendis aspernatur accusantium tempora consequatur dolor velit dolorum officiis debitis harum sit quidem, temporibus molestiae dignissimos totam molestias dolores libero doloribus impedit eveniet alias nam. Placeat odit explicabo consequatur sunt. Nisi eveniet impedit libero, doloremque explicabo esse modi perspiciatis odit expedita, exercitationem, nam amet laborum? Et nostrum itaque dolorem maiores distinctio optio ex vero neque, quos perferendis illum ullam voluptatum, consequatur modi quod natus? Laboriosam exercitationem facere, nihil quod reiciendis fugiat veritatis doloribus quibusdam enim possimus maiores voluptatibus! Veniam, molestias vel dolorum modi deleniti illo dolore officiis, necessitatibus vero suscipit omnis voluptatibus nam odio? Numquam pariatur sequi repellendus necessitatibus distinctio iure esse autem architecto ratione quam earum deserunt alias, maiores consequuntur magni sapiente, est quia exercitationem suscipit voluptas libero voluptatum ipsum quasi. Dolores ducimus consequuntur repellat maiores a mollitia, aspernatur, sit, aut impedit ullam temporibus. Fugit maxime adipisci, eveniet molestiae iste ut recusandae repudiandae deleniti nam, voluptates alias consectetur esse incidunt animi. Ad aut temporibus at nam beatae praesentium repellat vitae ex quaerat minus harum adipisci minima maiores facilis quasi impedit, voluptas doloremque saepe delectus. Labore fuga doloribus hic dolore neque! Sapiente, fugiat quas fuga possimus dolorum itaque repellendus?
-                    </v-card-text>
-                </v-card>
+                
+                <teachers-account /> <!-- TEACHERS ACCOUNT SETTINGS -->
+
             </v-tab-item>
             <v-tab-item>
+
                 <dev-info /> <!-- DEVELOPERS INFORMATION -->
+
             </v-tab-item>
         </v-tabs>
     </div>
@@ -51,7 +55,8 @@ export default {
     components: {
         Account: () => import('./admin-account/Account'),
         Administrator: () => import('./admin-profiles/Admin'),
-        DevInfo: () => import('./developers-info/DevInfo')
+        DevInfo: () => import('./developers-info/DevInfo'),
+        TeachersAccount: () => import('./teachers-account/TeachersAccount')
     }
 }
 </script>
