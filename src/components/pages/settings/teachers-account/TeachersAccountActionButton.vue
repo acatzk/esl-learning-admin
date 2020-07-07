0c8d48cb757a15a0873d356bb067bc47707c9539
acatzk <joshuaimalay@gmail.com>
2020-07-07 16:24:37 +0800

    Added action-button for deleting account event

---
+++ src/components/pages/settings/teachers-account/TeachersAccountActionButton.vue
@@ -0,0 +1,35 @@
+<template>
+    <div class="action-button">
+        <v-btn 
+            small
+            icon
+            color="error"
+        >
+            <v-icon>delete_outline</v-icon>
+        </v-btn>
+    </div>
+</template>
+
+
+<script>
+
+// Toast Alert Status file
+import { toastAlertStatus } from '@/assets/js/toastAlert'
+
+export default {
+    name: 'TeachersAccountActionButton',
+
+    data () {
+        return {
+            dialog: false
+        }
+    },
+
+    props: ['item'],
+
+    methods: {
+      
+    }
+
+}
+</script>
