81fd79a231e20e79e4634f16331e4de1339ea02c
acatzk <joshuaimalay@gmail.com>
2020-07-08 07:55:34 +0800

    Added v-lazy-image built in vue component

---
+++ src/components/pages/settings/developers-info/BlurImage.vue
@@ -0,0 +1,18 @@
+<template functional>
+  <v-lazy-image
+    :src="props.src"
+    :src-placeholder="props.srcPlaceholder"
+    alt="Periscope"
+  ></v-lazy-image>
+</template>
+
+<style scoped>
+.v-lazy-image {
+  filter: blur(5px);
+  transition: filter 1.6s;
+  will-change: filter;
+}
+.v-lazy-image-loaded {
+  filter: blur(0);
+}
+</style>
