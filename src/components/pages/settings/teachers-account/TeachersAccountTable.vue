ff136b92d05fcb53b50d6d4bf9cf697c09449cdf
acatzk <joshuaimalay@gmail.com>
2020-07-07 16:23:45 +0800

    Added TeachersTable for displayed information

---
+++ src/components/pages/settings/teachers-account/TeachersAccountTable.vue
@@ -0,0 +1,92 @@
+<template>
+    <v-data-table
+        :items="items"
+        :headers="headers"
+        :search="search"
+    >
+        <!-- ** TEACHER AVATAR  ** -->
+        <template #item.firstname="{ item }">
+            <v-icon left>mdi-account</v-icon>
+            <span>{{ capitalize(item.firstname) + " " + capitalize(item.lastname) }}</span>
+        </template>
+
+        <!-- ** EMAIL ** -->
+        <template #item.email="{ item }">
+            <span>
+                <v-icon left>mdi-email</v-icon>
+                {{ item.email }}
+            </span>
+        </template>
+
+        <!-- ** CONTACT ** -->
+        <template #item.phone="{ item }">
+            <span>
+                <v-icon left>mdi-phone</v-icon>
+                {{ item.phone }}
+            </span>
+        </template>
+
+        <!-- ** GENDER ** -->
+        <template #item.gender="{ item }">
+            <td @click.stop class="non-clickable">
+                <v-icon left v-show="item.gender === 'Female'">mdi-gender-female</v-icon> 
+                <v-icon left v-show="item.gender === 'Male'">mdi-gender-male</v-icon> 
+                {{ item.gender === 'Male' ? 'Male' : 'Female' }}
+            </td>
+        </template>
+
+
+         <!-- ** ACTION BUTTONS ** -->
+        <template #item.id="{ item }">  
+            <teachers-account-action-button 
+                :item="item"
+            />
+        </template>
+
+    </v-data-table>
+</template>
+
+<script>
+
+export default {
+    name: 'TeachersAccountTable',
+
+    components: {
+        TeachersAccountActionButton: () => import('./TeachersAccountActionButton')
+    },
+
+    props: {
+        items: {
+            type: Array,
+            required: true
+        },
+        headers: {
+            type: Array,
+            requried: true
+        },
+        search: {
+            type: String,
+            required: true
+        }
+    },
+
+    methods: {
+        capitalize(s) {
+            if (typeof s !== 'string') return ''
+            return s.charAt(0).toUpperCase() + s.slice(1)
+        }
+    }
+}
+</script>
+
+<style scoped>
+.v-btn {
+    text-transform: capitalize;
+}
+.v-icon {
+    font-size: 16px;
+}
+::v-deep .v-data-table-header{
+  text-transform: uppercase;
+}
+</style>
