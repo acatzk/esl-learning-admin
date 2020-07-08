2fb4d137a030eea8c287f7139a24f79423d424ed
acatzk <joshuaimalay@gmail.com>
2020-07-09 07:03:20 +0800

    Removed font-family nunito imported

--- src/App.vue
+++ src/App.vue
@@ -4,14 +4,7 @@
   </v-app>
 </template>
 
-<style lang="scss">
-@import url("https://fonts.googleapis.com/css?family=Nunito:100,300,400,500,700,900");
-
-body, html {
-  font-family: "Nunito";
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-}
+<style>
 #nprogress .bar {
   background: rgb(198, 186, 255) !important;
 }
