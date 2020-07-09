fbf8739668370ebed97f61e25fd6d2304db862e1
acatzk <joshuaimalay@gmail.com>
2020-07-09 15:17:01 +0800

    Added loading state

--- src/views/admin/FileUpload.vue
+++ src/views/admin/FileUpload.vue
@@ -6,6 +6,7 @@
     prepend-icon="mdi-camera"
     @change="uploadImage"
     label="Avatar"
+    :loading="loading"
   ></v-file-input>
 </template>
 
@@ -13,16 +14,31 @@
 
 import { fb } from '@/firebase'
 
+import { toastAlertStatus } from '@/assets/js/toastAlert'
+
 export default {
     data: () => ({
         rules: [
         value => !value || value.size < 2000000 || 'Avatar size should be less than 2 MB!',
         ],
+        loading: false
     }),
     methods: {
         uploadImage (file) {
+            if (!file) return;
+
+            this.loading = true
             let storageRef = fb.storage().ref('admin-profiles/' + file.name)
-            storageRef.put(file)
+            let uploadTask = storageRef.put(file)
+
+            uploadTask.on('state_changed', (snapshot) => {
+                uploadTask.snapshot.ref.getDownloadURL()
+                    .then(downloadUrl => {
+                        console.log('File available at: ', downloadUrl)
+                        toastAlertStatus('success', `Uploaded`)
+                        this.loading = false
+                    })
+            })
         }
     }
 }
