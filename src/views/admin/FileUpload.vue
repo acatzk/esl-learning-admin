000d08ec1d0656898a7eb84c8f9199f7e6c83131
acatzk <joshuaimalay@gmail.com>
2020-07-10 06:59:45 +0800

    Fixed the loading file upload set to false

--- src/views/admin/FileUpload.vue
+++ src/views/admin/FileUpload.vue
@@ -1,13 +1,15 @@
 <template>
-  <v-file-input
-    :rules="rules"
-    accept="image/png, image/jpeg, image/bmp"
-    placeholder="Pick an avatar"
-    prepend-icon="mdi-camera"
-    @change="uploadImage"
-    label="Avatar"
-    :loading="loading"
-  ></v-file-input>
+  <div>
+       <v-file-input
+        :rules="rules"
+        accept="image/png, image/jpeg, image/bmp"
+        placeholder="Pick an avatar"
+        prepend-icon="mdi-camera"
+        @change="uploadImage"
+        label="Avatar"
+        :loading="loading"
+    ></v-file-input>
+  </div>
 </template>
 
 <script>
@@ -26,26 +28,25 @@ export default {
     methods: {
         uploadImage (file) {
             if (!file) return;
+            this.loading = true
 
             let storageRef = fb.storage().ref('admin-profiles/' + file.name)
             let uploadTask = storageRef.put(file)
 
-            uploadTask.on('state_changed', function(snapshot){
+            uploadTask.on('state_changed', (snapshot) => {
                     let progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
                     console.log('Upload is ' + progress + '% done');
-                    
-                }, function(error) {
+                }, (error) => {
                     // Handle unsuccessful uploads
                     toastAlertStatus('error', error)
-                }, function() {
+                }, () => {
                     // Handle successful uploads on complete
                     // For instance, get the download URL: https://firebasestorage.googleapis.com/...
-                    this.loading = true
+                    this.loading = false
                     uploadTask.snapshot.ref.getDownloadURL()
                         .then(downloadUrl => {
                             console.log('File available at: ', downloadUrl)
-                            toastAlertStatus('success', `Uploaded`)
-                            this.loading = false
+                            toastAlertStatus('success', `Uploaded ${downloadUrl}`)
                         })
                         .catch(error => toastAlertStatus('error', error))
             });
