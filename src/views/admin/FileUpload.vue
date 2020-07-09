3e6220dc947c0f786a3de1a9418c1a20c4fe5201
acatzk <joshuaimalay@gmail.com>
2020-07-09 14:48:08 +0800

    Added upImage image onChange method and upload to firebase

--- src/views/admin/FileUpload.vue
+++ src/views/admin/FileUpload.vue
@@ -4,16 +4,26 @@
     accept="image/png, image/jpeg, image/bmp"
     placeholder="Pick an avatar"
     prepend-icon="mdi-camera"
+    @change="uploadImage"
     label="Avatar"
   ></v-file-input>
 </template>
 
 <script>
-  export default {
+
+import { fb } from '@/firebase'
+
+export default {
     data: () => ({
-      rules: [
+        rules: [
         value => !value || value.size < 2000000 || 'Avatar size should be less than 2 MB!',
-      ],
+        ],
     }),
-  }
+    methods: {
+        uploadImage (file) {
+            let storageRef = fb.storage().ref('admin-profiles/' + file.name)
+            storageRef.put(file)
+        }
+    }
+}
 </script>
