d53e8e2f0038a20874f565cc595e5f12da4eed4f
acatzk <joshuaimalay@gmail.com>
2020-07-10 08:08:59 +0800

    Uploaded avatar image in firebase success

--- src/components/pages/profile/AvatarDialog.vue
+++ src/components/pages/profile/AvatarDialog.vue
@@ -1,6 +1,6 @@
 <template>
     <v-row justify="center">
-        <v-dialog v-model="show" max-width="600px">
+        <v-dialog v-model="show" max-width="450px">
             <v-card>
                 <v-card-title>
                     <v-icon left size="35">mdi-camera-outline</v-icon> Upload Avatar
@@ -13,18 +13,49 @@
                             v-model="valid"
                             lazy-validation
                         >
-                            <v-row>
-                                <v-col cols="12" sm="6" md="6">
-                                    Avatar
-                                </v-col>
-                            </v-row>
+                            <v-layout>
+                                <v-flex>
+                                    <v-img
+                                        :src="previewImage(image)"
+                                        :lazy-src="previewImage(image)"
+                                        aspect-ratio="1"
+                                        class="grey lighten-2"
+                                        height="50vh"
+                                    >
+                                        <template v-slot:placeholder>
+                                            <v-row
+                                            class="fill-height ma-0"
+                                            align="center"
+                                            justify="center"
+                                            >
+                                            <v-progress-circular indeterminate color="grey lighten-5"></v-progress-circular>
+                                            </v-row>
+                                        </template>
+                                    </v-img>
+                                </v-flex>
+                            </v-layout>
+                            <v-layout>
+                                <v-row>
+                                    <v-col cols="12" >
+                                        <v-file-input
+                                            :rules="rules"
+                                            accept="image/png, image/jpeg, image/bmp"
+                                            placeholder="Pick an avatar"
+                                            prepend-icon="mdi-camera-iris"
+                                            @change="uploadImage"
+                                            label="Avatar"
+                                            :loading="loading"
+                                        ></v-file-input>
+                                    </v-col>
+                                </v-row>
+                            </v-layout>
                         </v-form>
                     </v-container>
                 </v-card-text>
                 <v-card-actions>
                     <v-spacer></v-spacer>
                     <v-btn 
-                        color="indigo darken-1 white--text" 
+                        color="primary white--text" 
                         depressed
                         :loading="loading"  
                         small
@@ -38,6 +69,11 @@
 </template>
 
 <script>
+
+import { fb } from '@/firebase'
+
+import { toastAlertStatus } from '@/assets/js/toastAlert'
+
 export default {
     
     props: ['visible', 'admins'],
@@ -46,7 +82,11 @@ export default {
         return {
             menu: false,
             loading: false,
-            valid: true
+            valid: true,
+            rules: [
+                value => !value || value.size < 2000000 || 'Avatar size should be less than 2 MB!',
+            ],
+            image: null
         }
     },
 
@@ -60,6 +100,42 @@ export default {
                     this.$emit('close')
                 }
             }
+        }
+    },
+
+    methods: {
+        uploadImage (file) {
+
+            if (!file) return;
+
+            this.loading = true
+            let storageRef = fb.storage().ref('admin-profiles/' + file.name)
+            let uploadTask = storageRef.put(file)
+            uploadTask.on('state_changed', (snapshot) => {
+                    let progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100
+                    console.log('Upload is ' + progress + '% done')
+                }, (error) => {
+                    // Handle unsuccessful uploads
+                    toastAlertStatus('error', error)
+                }, () => {
+                    // Handle successful uploads on complete
+                    // For instance, get the download URL: https://firebasestorage.googleapis.com/...
+                    this.loading = false
+                    uploadTask.snapshot.ref.getDownloadURL()
+                        .then(downloadUrl => {
+                            this.image = downloadUrl
+                            toastAlertStatus('success', `Uploaded ${downloadUrl}`)
+                        })
+                        .catch(error => toastAlertStatus('error', error))
+            })
+        },
+
+        previewImage(image) {
+            if (image === null) {
+                return image = 'https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcQxs9QORl3noSnnXUQaU_Vlt3pbxfSy718YOuSIY3d3O69t3FeF&usqp=CAU'
+             } else {
+                 return image
+             }            
         }
     }
 }
