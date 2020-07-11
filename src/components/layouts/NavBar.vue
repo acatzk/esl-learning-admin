3fd992b2f088adb0645eba0e778b71fa231d3bc8
acatzk <joshuaimalay@gmail.com>
2020-07-11 16:41:26 +0800

    Added temporary notifications with dense v-list

--- src/components/layouts/NavBar.vue
+++ src/components/layouts/NavBar.vue
@@ -39,10 +39,54 @@
                 <v-icon>mdi-apps</v-icon>
             </v-btn>
 
-            <v-btn icon>
-                <v-icon>mdi-bell</v-icon>
-            </v-btn>
 
+            <!-- Inbox Notifications -->
+            <v-menu offset-y transition="slide-x-transition">
+                <template v-slot:activator="{ on, attrs }">
+                    <v-btn 
+                        icon
+                        v-bind="attrs"
+                        v-on="on"
+                    >
+                        <v-icon>mdi-bell</v-icon>
+                    </v-btn>
+                </template>
+                
+                <v-list>
+                    <v-list three-line dense style="max-width: 250px;">
+                        <template v-for="(item, index) in notifications">
+                            <v-subheader
+                                v-if="item.header"
+                                :key="item.header"
+                                v-text="item.header"
+                            ></v-subheader>
+
+                            <v-divider
+                                v-else-if="item.divider"
+                                :key="index"
+                                :inset="item.inset"
+                            ></v-divider>
+
+                            <v-list-item
+                                v-else
+                                :key="item.title"
+                                dense
+                            >
+                                <v-list-item-avatar>
+                                    <v-img :src="item.avatar"></v-img>
+                                </v-list-item-avatar>
+
+                                <v-list-item-content>
+                                    <v-list-item-title v-html="item.title"></v-list-item-title>
+                                    <v-list-item-subtitle v-html="item.subtitle"></v-list-item-subtitle>
+                                </v-list-item-content>
+                            </v-list-item>
+                        </template>
+                    </v-list>
+                </v-list>
+          </v-menu>
+
+            <!-- Admin Avatar Profile -->
             <v-menu offset-y transition="slide-x-transition">
                 <template v-slot:activator="{ on, attrs }">
                     <v-btn 
@@ -72,7 +116,7 @@
                     </v-btn>
                 </template>
 
-                <v-list>
+                <v-list dense>
                     <v-list-item
                         v-for="(opt, io) in profileOptions" 
                         :key="io"
@@ -89,7 +133,6 @@
                            <v-icon left>mdi-logout</v-icon> Logout
                         </v-list-item-title>
                     </v-list-item>
-
                 </v-list>
           </v-menu>
             
@@ -117,6 +160,52 @@ export default {
             profileOptions: [
                 { icon: 'mdi-account-outline', text: 'Profile', to: `/admin/profile/${fb.auth().currentUser.uid}` },
                 { icon: 'mdi-earth', text: 'Settings', to: '/admin/settings' }
+            ],
+            notifications: [
+                { 
+                    header: 'Today' 
+                },
+                {
+                    avatar: 'https://cdn.vuetifyjs.com/images/lists/1.jpg',
+                    title: 'Brunch this weekend?',
+                    subtitle: "<span class='text--primary'>Ali Connors</span> &mdash; I'll be in your neighborhood doing errands this weekend. Do you want to hang out?",
+                },
+                { 
+                    divider: true, 
+                    inset: true 
+                },
+                {
+                    avatar: 'https://cdn.vuetifyjs.com/images/lists/2.jpg',
+                    title: 'Summer BBQ <span class="grey--text text--lighten-1">4</span>',
+                    subtitle: "<span class='text--primary'>to Alex, Scott, Jennifer</span> &mdash; Wish I could come, but I'm out of town this weekend.",
+                },
+                { 
+                    divider: true, 
+                    inset: true 
+                },
+                {
+                    avatar: 'https://cdn.vuetifyjs.com/images/lists/3.jpg',
+                    title: 'Oui oui',
+                    subtitle: "<span class='text--primary'>Sandra Adams</span> &mdash; Do you have Paris recommendations? Have you ever been?",
+                },
+                { 
+                    divider: true, 
+                    inset: true 
+                },
+                {
+                    avatar: 'https://cdn.vuetifyjs.com/images/lists/4.jpg',
+                    title: 'Birthday gift',
+                    subtitle: "<span class='text--primary'>Trevor Hansen</span> &mdash; Have any ideas about what we should get Heidi for her birthday?",
+                },
+                { 
+                    divider: true, 
+                    inset: true 
+                },
+                {
+                    avatar: 'https://cdn.vuetifyjs.com/images/lists/5.jpg',
+                    title: 'Recipe to try',
+                    subtitle: "<span class='text--primary'>Britta Holt</span> &mdash; We should eat this: Grate, Squash, Corn, and tomatillo Tacos.",
+                },
             ]
         }
     },
