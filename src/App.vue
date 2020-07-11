b7bfcdad5d4a8a905047dcf0bc6fbe0d6107a748
acatzk <joshuaimalay@gmail.com>
2020-07-11 09:13:42 +0800

    Changed font family nunito

--- src/App.vue
+++ src/App.vue
@@ -6,11 +6,19 @@
 
 <style lang="scss">
 @import url("https://fonts.googleapis.com/css?family=Nunito:100,300,400,500,700,900");
+
+.v-application {
+  font-family: "Nunito", sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+}
+
 body, html {
   font-family: "Nunito", sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
 }
+
 #nprogress .bar {
   background: rgb(198, 186, 255) !important;
 }
