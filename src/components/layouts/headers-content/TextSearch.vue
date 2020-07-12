57ce2e4ebd6cf866151c9bcf5f9cc53b87618661
acatzk <joshuaimalay@gmail.com>
2020-07-12 13:20:37 +0800

    Added v-text-field for search and passed props

---
+++ src/components/layouts/headers-content/TextSearch.vue
@@ -0,0 +1,20 @@
+<template>
+    <v-text-field
+        solo-inverted
+        flat
+        tile
+        hide-details
+        label="Search"
+        append-icon="search"
+        :color="mode ? 'deep-purple darken-4' : ''"
+        dense
+    ></v-text-field>
+</template>
+
+<script>
+export default {
+    name: 'TextSearch',
+
+    props: ['mode']
+}
+</script>
