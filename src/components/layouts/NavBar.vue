e7b5ee16736619722f24786859a020f9e59cd612
acatzk <joshuaimalay@gmail.com>
2020-07-12 08:47:25 +0800

    Import ProfileOptions vue component

--- src/components/layouts/NavBar.vue
+++ src/components/layouts/NavBar.vue
@@ -103,54 +103,7 @@
             </v-menu>
 
             <!-- Admin Avatar Profile -->
-            <v-menu offset-y transition="slide-x-transition">
-                <template v-slot:activator="{ on, attrs }">
-                    <v-btn 
-                        v-if="$apollo.loading" 
-                        icon 
-                        :loading=true
-                    >
-                    </v-btn>
-                    <v-btn
-                        v-else
-                        icon
-                        large
-                        v-bind="attrs"
-                        v-on="on"
-                    >
-                        <v-avatar
-                            size="32px"
-                            item
-                            color="indigo lighten-1"
-                        >
-                            <blur-image 
-                                v-for="(pro, index) in profile" :key="index"
-                                :src="profileImage(pro)"
-                                alt="profile"
-                            />
-                        </v-avatar>
-                    </v-btn>
-                </template>
-
-                <v-list dense class="mt-3">
-                    <v-list-item
-                        v-for="(opt, io) in profileOptions" 
-                        :key="io"
-                        :to="opt.to"
-                        color="indigo darken-1"
-                    >
-                        <v-list-item-title>
-                            <v-icon left>{{ opt.icon }}</v-icon> {{ opt.text }}
-                        </v-list-item-title>
-                    </v-list-item>
-
-                    <v-list-item color="indigo darken-1" @click="adminLogout">
-                        <v-list-item-title>
-                            <v-icon left>mdi-logout</v-icon> Logout
-                        </v-list-item-title>
-                    </v-list-item>
-                </v-list>
-            </v-menu>
+            <profile-options />
             
         </v-app-bar>
 
@@ -188,18 +141,11 @@ export default {
 
     components: {
         SideBar: () => import('./SideBar'),
-        BlurImage: () => import('@/components/pages/settings/developers-info/BlurImage'),
-        DateDisplay: () => import('@/components/pages/DateDisplay')
+        DateDisplay: () => import('@/components/pages/DateDisplay'),
+        ProfileOptions: () => import('./headers-content/ProfileOptions')
     },
 
     methods: {
-        profileImage (profile) {
-            if (profile.profileUrl === null) {
-                return 'https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcQxs9QORl3noSnnXUQaU_Vlt3pbxfSy718YOuSIY3d3O69t3FeF&usqp=CAU'
-            } else {
-                return profile.profileUrl
-            }
-        },
         adminLogout () {
             fb.auth()
               .signOut()
