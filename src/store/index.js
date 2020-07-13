0737a79309a4bc30b8907f452d20d9cd73cf7cc6
acatzk <joshuaimalay@gmail.com>
2020-07-13 08:08:54 +0800

    Added vuex state for dark mode mutation and actions

--- src/store/index.js
+++ src/store/index.js
@@ -4,8 +4,18 @@ import Vuex from "vuex";
 Vue.use(Vuex);
 
 export default new Vuex.Store({
-  state: {},
-  mutations: {},
-  actions: {},
+  state: {
+    mode: false
+  },
+  mutations: {
+    DARK_MODE({ state }, mode) {
+      state.mode = !mode
+    }
+  },
+  actions: {
+    darkMode({ commit }, mode) {
+      commit('DARK_MODE', mode)
+    }
+  },
   modules: {}
 });
