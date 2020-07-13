a00336392b68c4fbb2ef8573ec5be9ec27853223
acatzk <joshuaimalay@gmail.com>
2020-07-13 12:31:23 +0800

    Displayed teachers profile image

--- src/components/pages/settings/teachers-account/TeachersAccountTable.vue
+++ src/components/pages/settings/teachers-account/TeachersAccountTable.vue
@@ -6,7 +6,17 @@
     >
         <!-- ** TEACHER AVATAR  ** -->
         <template #item.firstname="{ item }">
-            <v-icon left>mdi-account</v-icon>
+            <v-avatar 
+                color="#eee" 
+                class="mr-2 ma-1 avatar-pic" 
+                @click="dialog = !dialog"
+            >
+                <img
+                    :src="teacherProfileImage(item)"
+                    alt="profile-url"
+
+                />     
+            </v-avatar>
             <span>{{ capitalize(item.firstname) + " " + capitalize(item.lastname) }}</span>
         </template>
 
@@ -71,6 +81,13 @@ export default {
     },
 
     methods: {
+        teacherProfileImage (profile) {
+            if (profile.profile_url === null) {
+                return 'https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcQxs9QORl3noSnnXUQaU_Vlt3pbxfSy718YOuSIY3d3O69t3FeF&usqp=CAU'
+            } else {
+                return profile.profile_url
+            }
+        },
         capitalize(s) {
             if (typeof s !== 'string') return ''
             return s.charAt(0).toUpperCase() + s.slice(1)
