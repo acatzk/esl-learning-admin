5ec69367f4dfb2a2dcaab1b24dd92f9ac5eb1b44
acatzk <joshuaimalay@gmail.com>
2020-07-14 17:32:46 +0800

    Added dialog for search event

--- src/components/layouts/headers-content/ButtonSearch.vue
+++ src/components/layouts/headers-content/ButtonSearch.vue
@@ -1,7 +1,43 @@
 <template>
-    <v-btn 
-        icon
-    >
-        <v-icon>search</v-icon>
-    </v-btn>
+   <div>
+        <v-btn 
+            icon
+            @click="dialog = !dialog"
+        >
+            <v-icon>search</v-icon>
+        </v-btn>
+
+        <v-dialog
+            v-model="dialog"
+            fullscreen
+            hide-overlay
+            transition="dialog-bottom-transition"
+            scrollable
+        >
+            <v-card flat>
+                <v-row>
+                    <v-col cols="12">
+                        <v-btn icon @click="dialog = !dialog" large>
+                            <v-icon>chevron_left</v-icon>
+                        </v-btn>
+                        <v-text-field>
+                            Search..
+                        </v-text-field>
+                    </v-col>
+                </v-row>
+            </v-card>
+        </v-dialog>
+   </div>
 </template>
+
+<script>
+export default {
+    name: 'ButtonSearch',
+
+    data () {
+        return {
+            dialog: false
+        }
+    }
+}
+</script>
