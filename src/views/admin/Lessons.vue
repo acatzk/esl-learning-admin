ee7d5fa43f2edc239ca5b249d7ef5045bd58b8be
acatzk <joshuaimalay@gmail.com>
2020-07-14 15:37:07 +0800

    Added v-card and text-field for search in table

--- src/views/admin/Lessons.vue
+++ src/views/admin/Lessons.vue
@@ -4,18 +4,30 @@
             <v-toolbar-title>
                 <v-icon right>mdi-folder-multiple-outline</v-icon> Lessons
             </v-toolbar-title>
-
             <v-btn small depressed color="primary" @click.stop="dialog = true">
                 <v-icon left>mdi-plus</v-icon> New
             </v-btn>
-
         </header>
         
         <lesson-dialog 
             :visible="dialog" @close="dialog = false"
         />
 
-        
+         <v-card flat>
+           
+            <!-- ** SEARCH TEXT FIELD ** -->
+            <v-text-field
+                placeholder="Search"
+                v-model="search"
+                filled
+                class="mx-3"
+                prepend-inner-icon="search"
+                rounded
+                style="position: relative; top: 17px; border-radius: 50px;"
+            ></v-text-field>
+
+        </v-card>
+
     </div>
 </template>
 
@@ -29,7 +41,8 @@ export default {
 
     data () {
         return {
-            dialog: false
+            dialog: false,
+            search: ''
         }
     }
 }
