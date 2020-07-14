5ca39033e581899fb4905946e2eb24964eee91c1
acatzk <joshuaimalay@gmail.com>
2020-07-14 16:16:08 +0800

    Displayed items file and added icon

---
+++ src/components/pages/lessons/LessonsTable.vue
@@ -0,0 +1,63 @@
+<template>
+    <v-data-table
+        :items="items"
+        :headers="headers"
+        :search="search"
+        show-expand
+    >
+        <!-- TITLE DISPLAY -->
+        <template #item.title="{ item }">
+            <span class="d-flex align-center">
+                <v-icon left>mdi-file-document</v-icon>
+                {{ item.title }}
+            </span>
+        </template>
+
+        <!-- FILENAME DISPLAY -->
+        <template #item.filename="{ item }">
+            <span class="d-flex align-center">
+                <v-icon left>mdi-file-pdf-box</v-icon>
+                <a href="#" target="_blank" style="text-decoration: none;">
+                    {{ item.filename }}
+                </a>
+            </span>
+        </template>
+
+        <!-- ** EXPANDED MESSAGE AND ACTION BUTTONS ** -->
+        <template #expanded-item="{ headers, item }">
+            <td :colspan="headers.length">
+                <v-row>
+                    <v-col cols="12" md="2">
+                        <b>{{ item.title }}</b>
+                    </v-col>
+                    <v-col cols="12" md="10">
+                        <p>{{ item.description }}</p>
+                    </v-col>
+                </v-row>
+            </td>
+        </template>
+        <!-- ** END EXPANDED MESSAGE AND ACTION BUTTONS ** -->
+    </v-data-table>
+</template>
+
+<script>
+export default {
+    name: 'LessonsTable',
+
+    props: {
+        items: {
+            type: Array,
+            required: true
+        },
+        headers: {
+            type: Array,
+            requried: true
+        },
+        search: {
+            type: String,
+            required: true
+        }
+    },
+
+}
+</script>
