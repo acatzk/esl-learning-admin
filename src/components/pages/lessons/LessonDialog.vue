4a208f55da93e0ee92b864ffc3960cf175b745ba
acatzk <joshuaimalay@gmail.com>
2020-07-14 15:24:08 +0800

    Passed prop visible and added computed show and hide boolean

---
+++ src/components/pages/lessons/LessonDialog.vue
@@ -0,0 +1,74 @@
+<template>
+    <v-row justify="center">
+        <v-dialog v-model="show" max-width="600px">
+            <v-card>
+                <v-card-title>
+                    <span class="headline">
+                        <v-icon left size="35">mdi-plus</v-icon> New Lesson
+                    </span>
+                </v-card-title>
+                <v-card-text>
+                    <v-container>
+                         <v-form 
+                            :disabled="loading" 
+                            ref="form"
+                            v-model="valid"
+                            lazy-validation
+                        >
+                            
+                        </v-form>
+                    </v-container>
+                </v-card-text>
+                <v-card-actions>
+                    <v-btn 
+                        text 
+                        small
+                        color="grey" 
+                    >
+                        <v-icon left>mdi-reload</v-icon> Reset
+                    </v-btn>
+                    <v-spacer></v-spacer>
+                    <v-btn color="indigo darken-1" text small @click="show = !show">
+                        <v-icon left>mdi-close-circle-outline</v-icon> Close
+                    </v-btn>
+                    <v-btn 
+                        color="indigo darken-1 white--text" 
+                        small
+                        depressed
+                    >
+                      <v-icon left>mdi-content-save</v-icon>  Save
+                    </v-btn>
+                </v-card-actions>
+            </v-card>
+        </v-dialog> 
+    </v-row>
+</template>
+
+<script>
+export default {
+    name: 'LessonDialog',
+
+    props: ['visible'],
+
+    data () {
+        return {
+            loading: false,
+            date: null,
+            valid: true,
+        }
+    },
+
+    computed: {
+        show: {
+            get () {
+                return this.visible
+            },
+            set (value) {
+                if (!value) {
+                    this.$emit('close')
+                }
+            }
+        }
+    }
+}
+</script>
