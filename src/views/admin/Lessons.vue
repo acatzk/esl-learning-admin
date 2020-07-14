b2c691254100f7550188f3b6506b7593556ea234
acatzk <joshuaimalay@gmail.com>
2020-07-14 15:22:46 +0800

    Import LessonDialog vue component and passed prop visible

--- src/views/admin/Lessons.vue
+++ src/views/admin/Lessons.vue
@@ -1,19 +1,39 @@
 <template>
     <div class="lessons">
-        <header class="d-flex justify-space-between">
+        <header class="d-flex justify-space-between align-center">
             <v-toolbar-title>
                 <v-icon right>mdi-folder-multiple-outline</v-icon> Lessons
             </v-toolbar-title>
-            <v-btn small depressed color="primary">
+
+            <v-btn small depressed color="primary" @click.stop="dialog = true">
                 <v-icon left>mdi-plus</v-icon> New
             </v-btn>
 
-            <!-- <teacher-add-dialog 
-                :visible="dialog" @close="dialog = false"
-            /> -->
         </header>
+        
+        <lesson-dialog 
+            :visible="dialog" @close="dialog = false"
+        />
+
+        
     </div>
 </template>
+
+<script>
+export default {
+    name: 'Lessons',
+
+    components: {
+        LessonDialog: () => import('@/components/pages/lessons/LessonDialog')
+    },
+
+    data () {
+        return {
+            dialog: false
+        }
+    }
+}
+</script>
 
 
 <style scoped lang="scss">
