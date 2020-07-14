1f87188eea09c3de1cf75be467580b32a55f6682
acatzk <joshuaimalay@gmail.com>
2020-07-14 16:33:18 +0800

    Added v-btn-toggle for edit and delete

---
+++ src/components/pages/lessons/LessonActionButton.vue
@@ -0,0 +1,33 @@
+<template>
+    <div class="action-button">
+        <v-btn-toggle>
+            <v-tooltip top color="warning">
+                <template v-slot:activator="{ on, attrs }">
+                    <v-btn 
+                        v-bind="attrs"
+                        v-on="on"
+                        small 
+                        icon
+                    >
+                        <v-icon>mdi-pencil-box-outline</v-icon>
+                    </v-btn>
+                </template>
+                <span>Edit</span>
+            </v-tooltip>
+
+            <v-tooltip top color="error">
+                <template v-slot:activator="{ on, attrs }">
+                    <v-btn 
+                        v-bind="attrs"
+                        v-on="on"
+                        small
+                        icon
+                    >
+                        <v-icon>delete_outline</v-icon>
+                    </v-btn>
+                </template>
+                <span>Delete</span>
+            </v-tooltip>
+        </v-btn-toggle>
+    </div>
+</template>
