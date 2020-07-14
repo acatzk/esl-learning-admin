eb549573a3a845da943453dfd2321a40579d1b1a
acatzk <joshuaimalay@gmail.com>
2020-07-14 14:36:58 +0800

    Added header style with title and button new

--- src/views/admin/Lessons.vue
+++ src/views/admin/Lessons.vue
@@ -1,5 +1,31 @@
 <template>
     <div class="lessons">
-        <h1>Lessons</h1>
+        <header class="d-flex justify-space-between">
+            <v-toolbar-title>
+                <v-icon right>mdi-folder-multiple-outline</v-icon> Lessons
+            </v-toolbar-title>
+            <v-btn small depressed color="primary">
+                <v-icon left>mdi-plus</v-icon> New
+            </v-btn>
+
+            <!-- <teacher-add-dialog 
+                :visible="dialog" @close="dialog = false"
+            /> -->
+        </header>
     </div>
 </template>
+
+
+<style scoped lang="scss">
+.lessons {
+    header {
+        padding: 15px;
+        border-bottom: 1px solid #eee;
+    }
+    .v-btn-toggle {
+        .v-btn {
+            text-transform: capitalize;
+        }
+    }
+}
+</style>
