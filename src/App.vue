65602e727518f03665266241b40124ba3917f95e
acatzk <joshuaimalay@gmail.com>
2020-07-15 17:30:38 +0800

    Changed general fonts into Poppins

--- src/App.vue
+++ src/App.vue
@@ -6,15 +6,16 @@
 
 <style lang="scss">
 @import url("https://fonts.googleapis.com/css?family=Nunito:100,300,400,500,700,900");
+@import url('https://fonts.googleapis.com/css?family=Poppins:100,200,300,400,500,600,700,800,900&display=swap');
 
 .v-application {
-  font-family: "Nunito", sans-serif !important;
+  font-family: "Poppins", sans-serif !important;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
 }
 
 body, html {
-  font-family: "Nunito", sans-serif !important;
+  font-family: "Poppins", sans-serif !important;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
 }
