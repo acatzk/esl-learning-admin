cf7337e53dde7ec87892f0be620288fc4adce879
acatzk <joshuaimalay@gmail.com>
2020-07-17 18:10:43 +0800

    Changd the const name to plural

--- src/graphql/queries/lessons.js
+++ src/graphql/queries/lessons.js
@@ -1,6 +1,6 @@
 import gql from 'graphql-tag'
 
-export const LESSON_QUERY = gql`
+export const LESSONS_QUERY = gql`
     query LessonQuery {
         lessons(order_by: {created_at: desc}) {
             id
