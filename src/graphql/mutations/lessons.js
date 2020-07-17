68800040caf0fb5b058ca88a9ade59734ce4fbda
acatzk <joshuaimalay@gmail.com>
2020-07-17 17:53:31 +0800

    Changed the money to number in price

--- src/graphql/mutations/lessons.js
+++ src/graphql/mutations/lessons.js
@@ -1,7 +1,7 @@
 import gql from 'graphql-tag'
 
 export const ADD_LESSONS_MUTATION = gql`
-    mutation AddLessonMutation($title: String! $description: String, $price: money, $url_files: String!) {
+    mutation AddLessonMutation($title: String! $description: String, $price: numeric, $url_files: String!) {
         insert_lessons(objects: {title: $title, description: $description, price: $price, url_files: $url_files}) {
             affected_rows
             returning {
