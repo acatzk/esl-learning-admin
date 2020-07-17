79386e66025ab0fb36d72e18c426bf6e39a5a7fe
acatzk <joshuaimalay@gmail.com>
2020-07-17 18:10:13 +0800

    Added subscription lessons graphql api

---
+++ src/graphql/subscriptions/lessons.js
@@ -0,0 +1,14 @@
+import gql from 'graphql-tag'
+
+export const LESSONS_SUBSCRIPTION = gql`
+    subscription LessonSubscription {
+        lessons(order_by: {created_at: desc}) {
+            id
+            title
+            description
+            price
+            url_files
+            created_at
+        }
+    } 
+`
