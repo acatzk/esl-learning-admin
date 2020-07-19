106232331cffc660bc60f147b90f298a43dbf7cd
acatzk <joshuaimalay@gmail.com>
2020-07-19 10:42:52 +0800

    Added UpdateLessonMutation graphql api

--- src/graphql/mutations/lessons.js
+++ src/graphql/mutations/lessons.js
@@ -10,3 +10,14 @@ export const ADD_LESSONS_MUTATION = gql`
         }
     }
 `
+
+export const UPDATE_LESSONS_MUTATION = gql`
+    mutation UpdateLessonMutation($id: uuid!, $title: String, $description: String, $price: numeric, $url_files: String) {
+        update_lessons(where: {id: {_eq: $id}}, _set: {title: $title, description: $description, price: $price, url_files: $url_files}) {
+            affected_rows
+            returning {
+                id
+            }
+        }
+    }
+`
