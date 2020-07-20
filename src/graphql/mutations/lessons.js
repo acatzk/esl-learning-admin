2c17900655272294a35bf3c75d2f09aeaaba778a
acatzk <joshuaimalay@gmail.com>
2020-07-20 11:20:47 +0800

    Added size column for adding and updating in graphql mutation

--- src/graphql/mutations/lessons.js
+++ src/graphql/mutations/lessons.js
@@ -1,8 +1,8 @@
 import gql from 'graphql-tag'
 
 export const ADD_LESSONS_MUTATION = gql`
-    mutation AddLessonMutation($title: String! $description: String, $price: numeric, $url_files: String!) {
-        insert_lessons(objects: {title: $title, description: $description, price: $price, url_files: $url_files}) {
+    mutation AddLessonMutation($title: String! $description: String, $price: numeric, $url_files: String!, $size: numeric) {
+        insert_lessons(objects: {title: $title, description: $description, price: $price, url_files: $url_files, size: $size}) {
             affected_rows
             returning {
                 id
@@ -12,8 +12,8 @@ export const ADD_LESSONS_MUTATION = gql`
 `
 
 export const UPDATE_LESSONS_MUTATION = gql`
-    mutation UpdateLessonMutation($id: uuid!, $title: String, $description: String, $price: numeric, $url_files: String) {
-        update_lessons(where: {id: {_eq: $id}}, _set: {title: $title, description: $description, price: $price, url_files: $url_files}) {
+    mutation UpdateLessonMutation($id: uuid!, $title: String, $description: String, $price: numeric, $url_files: String, $size: numeric) {
+        update_lessons(where: {id: {_eq: $id}}, _set: {title: $title, description: $description, price: $price, url_files: $url_files, size: $size}) {
             affected_rows
             returning {
                 id
