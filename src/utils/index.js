2cf3f746673eba0e8a1ff8b4739374745ba08948
acatzk <joshuaimalay@gmail.com>
2020-07-22 13:38:29 +0800

    Import rules validation text-field

--- src/utils/index.js
+++ src/utils/index.js
@@ -1,11 +1,16 @@
 import { toastAlertStatus } from './toastAlert'
 import { capitalize } from './capitalize'
 import { splitUrlFilename, convertByteSize, fileExtentionIcon } from './lessons'
+import { required, emailRules, minLength, maxLength } from './validationRules'
 
 export {
     toastAlertStatus,
     capitalize,
     splitUrlFilename,
     convertByteSize,
-    fileExtentionIcon
+    fileExtentionIcon,
+    required, 
+    emailRules, 
+    minLength, 
+    maxLength
 }
