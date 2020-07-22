f823913e27e46018db010d8e787a81c3b8ac99aa
acatzk <joshuaimalay@gmail.com>
2020-07-22 13:36:48 +0800

    Changed the const to let in arrow functions and import object

--- src/utils/validationRules.js
+++ src/utils/validationRules.js
@@ -1,15 +1,22 @@
-export const required = (propertyType) => {
+let required = (propertyType) => {
     return v => v && v.length > 0 || `${propertyType} is required.`
 }
 
-export const emailRules = (propertyType) => {
+let emailRules = (propertyType) => {
     return v => /^\w+([.-]?\w+)*@\w+([.-]?\w+)*(\.\w{2,3})+$/.test(v) || `${propertyType} address must be valid.`
 }
 
-export const minLength = (propertyType, minLength) => {
+let minLength = (propertyType, minLength) => {
     return v => v && v.length >= minLength || `${propertyType} must be at least ${minLength} characters.`
 }
 
-export const maxLength = (propertyType, maxLength) => {
+let maxLength = (propertyType, maxLength) => {
     return v => v && v.length <= maxLength || `${propertyType} must be less than ${maxLength} characters.`
+}
+
+export {
+    required,
+    emailRules,
+    minLength,
+    maxLength
 }
