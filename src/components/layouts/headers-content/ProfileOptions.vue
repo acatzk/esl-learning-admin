72776622ed39b0b8804da1275d19790f352eefe9
acatzk <joshuaimalay@gmail.com>
2020-07-23 08:21:14 +0800

    Applied dark mode in v-list conditioned base on mode vuex state

--- src/components/layouts/headers-content/ProfileOptions.vue
+++ src/components/layouts/headers-content/ProfileOptions.vue
@@ -39,7 +39,7 @@
             </v-btn>
         </template>
 
-        <v-list dense class="mt-3">
+        <v-list dense class="mt-3" :dark="mode ? false : true">
             <v-list-item
                 v-for="(opt, io) in profileOptions" 
                 :key="io"
