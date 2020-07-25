66f529718399184a718277996efe5d0efb74e590
acatzk <joshuaimalay@gmail.com>
2020-07-25 19:52:46 +0800

    Changed let to const

--- src/utils/validationRules.js
+++ src/utils/validationRules.js
@@ -1,16 +1,16 @@
-let required = (propertyType) => {
+const required = (propertyType) => {
     return v => v && v.length > 0 || `${propertyType} is required.`
 }
 
-let emailRules = (propertyType) => {
+const emailRules = (propertyType) => {
     return v => /^\w+([.-]?\w+)*@\w+([.-]?\w+)*(\.\w{2,3})+$/.test(v) || `${propertyType} address must be valid.`
 }
 
-let minLength = (propertyType, minLength) => {
+const minLength = (propertyType, minLength) => {
     return v => v && v.length >= minLength || `${propertyType} must be at least ${minLength} characters.`
 }
 
-let maxLength = (propertyType, maxLength) => {
+const maxLength = (propertyType, maxLength) => {
     return v => v && v.length <= maxLength || `${propertyType} must be less than ${maxLength} characters.`
 }
 
