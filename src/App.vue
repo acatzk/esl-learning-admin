8bd0f825f3f3fdfb385b5af055e0e49b0fa8b15a
acatzk <joshuaimalay@gmail.com>
2020-07-26 09:36:17 +0800

    Customized the default vertical scrollbar and changed color as well as the progress bar color

--- src/App.vue
+++ src/App.vue
@@ -21,6 +21,26 @@ body, html {
 }
 
 #nprogress .bar {
-  background: rgb(198, 186, 255) !important;
+  background: #f09433; 
+  background: -moz-linear-gradient(45deg, #f09433 0%, #e6683c 25%, #dc2743 50%, #cc2366 75%, #bc1888 100%); 
+  background: -webkit-linear-gradient(45deg, #f09433 0%,#e6683c 25%,#dc2743 50%,#cc2366 75%,#bc1888 100%); 
+  background: linear-gradient(45deg, #f09433 0%,#e6683c 25%,#dc2743 50%,#cc2366 75%,#bc1888 100%); 
+  filter: progid:DXImageTransform.Microsoft.gradient( startColorstr='#f09433', endColorstr='#bc1888',GradientType=1 );
 }
+
+::-webkit-scrollbar {
+  width: 9px;
+}
+
+::-webkit-scrollbar-track {
+  border: 7px solid #272727;
+  box-shadow: inset 0 0 0 rgba(0, 0, 0, 0.5);
+}
+
+::-webkit-scrollbar-thumb {
+  background-color: #7f5a83;
+  background-image: linear-gradient(315deg, #7f5a83 0%, #0d324d 74%);
+  border-radius: 50px;
+}
+
 </style>
