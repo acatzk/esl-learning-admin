6647d69b5b317825e8debba6cdb46d4cdab6182a
acatzk <joshuaimalay@gmail.com>
2020-07-27 10:03:57 +0800

    Moved the app css inside the app.scss in assets folder

--- src/App.vue
+++ src/App.vue
@@ -1,46 +1,11 @@
 <template>
+
   <v-app>
+
     <router-view></router-view>
+
   </v-app>
+  
 </template>
 
-<style lang="scss">
-@import url("https://fonts.googleapis.com/css?family=Nunito:100,300,400,500,700,900");
-@import url('https://fonts.googleapis.com/css?family=Poppins:100,200,300,400,500,600,700,800,900&display=swap');
-
-.v-application {
-  font-family: "Poppins", sans-serif !important;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-}
-
-body, html {
-  font-family: "Poppins", sans-serif !important;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-}
-
-#nprogress .bar {
-  background: #f09433; 
-  background: -moz-linear-gradient(45deg, #f09433 0%, #e6683c 25%, #dc2743 50%, #cc2366 75%, #bc1888 100%); 
-  background: -webkit-linear-gradient(45deg, #f09433 0%,#e6683c 25%,#dc2743 50%,#cc2366 75%,#bc1888 100%); 
-  background: linear-gradient(45deg, #f09433 0%,#e6683c 25%,#dc2743 50%,#cc2366 75%,#bc1888 100%); 
-  filter: progid:DXImageTransform.Microsoft.gradient( startColorstr='#f09433', endColorstr='#bc1888',GradientType=1 );
-}
-
-::-webkit-scrollbar {
-  width: 9px;
-}
-
-::-webkit-scrollbar-track {
-  border: 7px solid #272727;
-  box-shadow: inset 0 0 0 rgba(0, 0, 0, 0.5);
-}
-
-::-webkit-scrollbar-thumb {
-  background-color: #7f5a83;
-  background-image: linear-gradient(315deg, #7f5a83 0%, #0d324d 74%);
-  border-radius: 50px;
-}
-
-</style>
+<style lang="scss" src="@/assets/app.scss"></style>
