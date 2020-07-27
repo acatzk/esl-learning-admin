845539967c2b7738d0ce5d3c244d4d64cacdbabe
acatzk <joshuaimalay@gmail.com>
2020-07-27 11:13:50 +0800

    Removed the usersFirebase config imported

--- src/services/index.js
+++ src/services/index.js
@@ -1,11 +1,9 @@
 import { apolloProvider } from './apollo'
 import { fb, db, auth, messaging, currentUser } from './adminFirebase'
-import { fb2 } from './usersFirebase'
 
 export {
     fb,
     db,
-    fb2,
     auth,
     messaging,
     currentUser,
