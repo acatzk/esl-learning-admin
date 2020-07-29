476cf66f400ebe665f1d09a85c87bf1d361f0300
acatzk <joshuaimalay@gmail.com>
2020-07-29 08:09:37 +0800

    Fixed the bugs in teachers firebase_id insert column

--- src/graphql/mutations/teachers.js
+++ src/graphql/mutations/teachers.js
@@ -2,7 +2,7 @@ import gql from 'graphql-tag'
 
 export const ADD_TEACHER_MUTATION = gql`
     mutation AddTeacherMutation($firstname: String!, $lastname: String!, $email: String!, $phone: String!, $gender: String!, $birth_date: String!, $firebase_id: String!) {
-        insert_teachers(objects: {firstname: $firstname, lastname: $lastname, email: $email, phone: $phone, gender: $gender, birth_date: $birth_date}, firebase_id: $firebase_id) {
+        insert_teachers(objects: {firstname: $firstname, lastname: $lastname, email: $email, phone: $phone, gender: $gender, birth_date: $birth_date, firebase_id: $firebase_id}) {
             affected_rows
             returning {
                 id
