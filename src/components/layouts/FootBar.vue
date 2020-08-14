9671905eb8cf22694f31137c1cb332ad1f2e1daf
acatzk <joshuaimalay@gmail.com>
2020-08-14 11:14:37 +0800

    Added text in footer section and linked the social buttons

--- src/components/layouts/FootBar.vue
+++ src/components/layouts/FootBar.vue
@@ -10,14 +10,26 @@
     >
       <v-card-text class="py-1 text-center">
         <v-btn
-          v-for="icon in icons"
-          :key="icon"
+          v-for="link in socialLinks"
+          :key="link"
           class="mx-4"
           dark
           icon
+          :href="link.link"
+          target="_blank"
         >
-          <v-icon size="24px" color="grey">{{ icon }}</v-icon>
+          <v-icon size="24px" color="grey">{{ link.icon }}</v-icon>
         </v-btn>
+      </v-card-text>
+
+      <v-card-text class="grey--text pt-0 text-justify font-weight-light">
+        We victors the dream and make globally competitive learners and professionals. Unleash the breakthrough English literacy for young learners with the most authentic teaching approach. Real time strengthening of Business and Professionals in doing strong partnership strategically mentored from chosen SME's and partners. At Victor Airclass Solutions "We Outsourced, Deliver, Transform lives in community.
+      </v-card-text>
+
+      <v-divider></v-divider>
+
+      <v-card-text class="grey--text">
+        {{ new Date().getFullYear() }} <span class="font-weight-medium">Air Class Solution, Inc.</span>
       </v-card-text>
     </v-card>
   </v-footer>
@@ -29,12 +41,24 @@ import { mapState } from 'vuex'
 
 export default {
   data: () => ({
-    icons: [
-      'mdi-facebook',
-      'mdi-twitter',
-      'mdi-linkedin',
-      'mdi-instagram',
-    ],
+    socialLinks: [
+      {
+        link: 'https://www.facebook.com/',
+        icon: 'mdi-facebook'
+      },
+      {
+        link: 'https://twitter.com/',
+        icon: 'mdi-twitter'
+      },
+      {
+        link: 'https://www.linkedin.com/',
+        icon: 'mdi-linkedin'
+      },
+      {
+        link: 'https://www.instagram.com/',
+        icon: 'mdi-instagram'
+      }
+    ]
   }),
 
   computed: {
